2c7cbc10eb08b0fb9a6698c28768ce469c3ead74
Peter Strøiman <peter@stroiman.com>
2025-02-16 21:20:13 +0100

    Improve package docs

--- dom/package.go
+++ dom/package.go
@@ -1,3 +1,11 @@
 //go:generate ../internal/code-gen/code-gen -g dom
 
+// Package dom provides the fundamental DOM implementation for Gost-DOM.
+//
+// The DOM includes a [Node] implementation, and it's fundamental special types,
+// such as [Element], [Document], [Text], etc; as well as events througn the
+// [EventTarget] type.
+//
+// Specific implementation of HTML element types, including the HTMLDocument, is
+// in the html package.
 package dom
--- html/package.go
+++ html/package.go
@@ -1,4 +1,6 @@
 //go:generate ../internal/code-gen/code-gen -g htmlelements
 
-// Package html works on top of [dom] to implement specific HTML elements
+// Package html works on top of the DOM to implement specific HTML elements.
+//
+// The fundamental structure is implemented in package [dom].
 package html
