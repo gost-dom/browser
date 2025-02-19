b9bc674894dc1cf4a555ff948280e1147ff22de5
Peter Strøiman <peter@stroiman.com>
2025-02-19 12:02:03 +0100

    Refactor URLSearchParams test to a suite
    Although a bit more code, seems a little bit more organised.

--- url/url_search_params_test.go
+++ url/url_search_params_test.go
@@ -5,108 +5,114 @@ import (
 
 	i "github.com/gost-dom/browser/internal/interfaces/url-interfaces"
 	. "github.com/gost-dom/browser/url"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestUrlSearchParamsConformsToInterface(t *testing.T) {
-	// Declare a var of the interface type to compile time check that the type
-	// conforms to the IDL spec as good as possible.
-	var u i.URLSearchParams = &URLSearchParams{}
-	assert.Equal(t, "", u.String())
+type URLSearchParamsSuits struct {
+	suite.Suite
+
+	// The test uses the implementation through the interface as this brings
+	// design-time validation that the type conforms to the IDL specs
+	u i.URLSearchParams
 }
 
-func TestUrlSearchParamsAddValue(t *testing.T) {
-	u := &URLSearchParams{}
-	u.Append("foo", "bar")
-	assert.Equal(t, "foo=bar", u.String())
-	v, found := u.Get("foo")
-	assert.True(t, found)
-	assert.Equal(t, "bar", v)
+func (s *URLSearchParamsSuits) SetupTest() {
+	s.u = &URLSearchParams{}
+}
+
+func (s *URLSearchParamsSuits) TestAddValue() {
+	s.u.Append("foo", "bar")
+	s.Assert().Equal("foo=bar", s.u.String())
+	v, found := s.u.Get("foo")
+	s.Assert().True(found)
+	s.Assert().Equal("bar", v)
 
 	// Same value can be added twice
-	u.Append("foo", "baz")
-	assert.Equal(t, "foo=bar&foo=baz", u.String())
-	assert.Equal(t, []string{"bar", "baz"}, u.GetAll("foo"))
+	s.u.Append("foo", "baz")
+	s.Assert().Equal("foo=bar&foo=baz", s.u.String())
+	s.Assert().Equal([]string{"bar", "baz"}, s.u.GetAll("foo"))
 
-	u = &URLSearchParams{}
-	u.Append("foo", "Bar value")
-	assert.Equal(t, "foo=Bar+value", u.String(), "Query encoding")
+	s.u = &URLSearchParams{}
+	s.u.Append("foo", "Bar value")
+	s.Assert().Equal("foo=Bar+value", s.u.String(), "Query encoding")
 }
 
-func TestGet(t *testing.T) {
-	u := &URLSearchParams{}
-	u.Append("foo", "bar")
+func (s *URLSearchParamsSuits) TestGet() {
+	s.u.Append("foo", "bar")
 
-	foundValue, ok := u.Get("foo")
-	assert.True(t, ok)
-	assert.Equal(t, "bar", foundValue)
+	foundValue, ok := s.u.Get("foo")
+	s.Assert().True(ok)
+	s.Assert().Equal("bar", foundValue)
 
-	missingValue, ok := u.Get("baz")
-	assert.False(t, ok)
-	assert.Equal(t, "", missingValue)
+	missingValue, ok := s.u.Get("baz")
+	s.Assert().False(ok)
+	s.Assert().Equal("", missingValue)
 }
 
-func TestParseUrlSearchParams(t *testing.T) {
+func (s *URLSearchParamsSuits) TestParse() {
 	// Test percent decoding
 	var u, err = ParseURLSearchParams("foo=bar%20value")
-	assert.NoError(t, err)
+	s.Assert().NoError(err)
 	v, found := u.Get("foo")
-	assert.True(t, found)
-	assert.Equal(t, "bar value", v)
+	s.Assert().True(found)
+	s.Assert().Equal("bar value", v)
 
 	// Test + decoding
 	u, err = ParseURLSearchParams("foo=bar+value")
-	assert.NoError(t, err)
+	s.Assert().NoError(err)
 	v, found = u.Get("foo")
-	assert.True(t, found)
-	assert.Equal(t, "bar value", v)
+	s.Assert().True(found)
+	s.Assert().Equal("bar value", v)
 
 	// Test leading ? is ignores
 	u, err = ParseURLSearchParams("?foo=bar%20value")
-	assert.NoError(t, err)
+	s.Assert().NoError(err)
 	v, found = u.Get("foo")
-	assert.True(t, found)
-	assert.Equal(t, "bar value", v)
+	s.Assert().True(found)
+	s.Assert().Equal("bar value", v)
 }
 
-func TestURLSearchParamsHas(t *testing.T) {
-	var u URLSearchParams
-	u.Append("foo", "bar")
-	assert.True(t, u.Has("foo"))
-	assert.False(t, u.Has("bar"))
+func (s *URLSearchParamsSuits) TestHas() {
+	s.u.Append("foo", "bar")
+	s.Assert().True(s.u.Has("foo"))
+	s.Assert().False(s.u.Has("bar"))
 
-	assert.True(t, u.HasValue("foo", "bar"))
-	assert.False(t, u.HasValue("foo", "baz"))
+	s.Assert().True(s.u.HasValue("foo", "bar"))
+	s.Assert().False(s.u.HasValue("foo", "baz"))
 }
 
-func TestURLSearchParamDelete(t *testing.T) {
-	var u URLSearchParams
+func (s *URLSearchParamsSuits) TestDelete() {
+	var u = s.u
 	u.Append("foo", "a")
 	u.Append("foo", "b")
 	u.Append("foo", "c")
 
 	u.DeleteValue("foo", "z")
-	assert.Equal(t, []string{"a", "b", "c"}, u.GetAll("foo"))
+	s.Assert().Equal([]string{"a", "b", "c"}, u.GetAll("foo"))
 
 	u.DeleteValue("foo", "c")
-	assert.Equal(t, []string{"a", "b"}, u.GetAll("foo"))
+	s.Assert().Equal([]string{"a", "b"}, u.GetAll("foo"))
 
 	u.Delete("foo")
 	_, found := u.Get("foo")
-	assert.False(t, found)
+	s.Assert().False(found)
 }
 
-func TestURLSearchParamsSet(t *testing.T) {
-	var u URLSearchParams
+func (s *URLSearchParamsSuits) TestSet() {
+	u := s.u
 
 	// Add a new value
 	u.Set("key1", "a")
 	val, found := u.Get("key1")
-	assert.True(t, found)
-	assert.Equal(t, "a", val)
+	s.Assert().True(found)
+	s.Assert().Equal("a", val)
 
 	u.Append("key2", "a")
 	u.Append("key2", "b")
 	u.Set("key2", "c")
-	assert.Equal(t, []string{"c"}, u.GetAll("key2"))
+	s.Assert().Equal([]string{"c"}, u.GetAll("key2"))
+}
+
+func TestURLSearchParams(t *testing.T) {
+	suite.Run(t, new(URLSearchParamsSuits))
 }
