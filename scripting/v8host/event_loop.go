eb936cb7aa33908bf0ea6053ffc0744416462b50
Peter Strøiman <peter@stroiman.com>
2025-02-21 13:22:56 +0100

    Remove goroutine for event loop

--- scripting/v8host/event_loop.go
+++ scripting/v8host/event_loop.go
@@ -1,9 +1,11 @@
 package v8host
 
 import (
+	"fmt"
 	"log/slog"
 	"runtime/debug"
 
+	"github.com/gost-dom/browser/clock"
 	"github.com/gost-dom/browser/internal/log"
 	v8 "github.com/tommie/v8go"
 )
@@ -13,10 +15,15 @@ type workItem struct {
 }
 
 type eventLoop struct {
+	clock        *clock.Clock
 	ctx          *V8ScriptContext
 	workItems    chan workItem
 	globalObject *v8.Object
 	errorCb      func(error)
+}
+
+func (l *eventLoop) tick() error {
+	return l.clock.Tick()
 }
 
 func newWorkItem(fn *v8.Function) workItem {
@@ -29,16 +36,36 @@ func (l *eventLoop) dispatch(w workItem) {
 	if !l.ctx.beginDispatch() {
 		return
 	}
-	go func() {
-		// Have seen issues with scripts being executed while trying to shut down
-		// v8. I want a better, channel based, message passing, solution. But if
-		// this works, it will for for a v 0.1 release
-		l.workItems <- w
-	}()
+	l.clock.AddSafeTask(clock.Immediate, func() {
+		fmt.Println("Execute function\n", w.fn.String())
+		if _, err := w.fn.Call(l.globalObject); err != nil {
+			fnAsString := w.fn.String()
+			log.Error(
+				"EventLoop: Error",
+				slog.String("script", fnAsString),
+				slog.String("error", err.Error()),
+				slog.String("stack", string(debug.Stack())),
+			)
+			l.errorCb(err)
+			l.errorCb(err)
+		}
+	})
+	// go func() {
+	// 	// Have seen issues with scripts being executed while trying to shut down
+	// 	// v8. I want a better, channel based, message passing, solution. But if
+	// 	// this works, it will for for a v 0.1 release
+	// 	l.workItems <- w
+	// }()
 }
 
 func newEventLoop(context *V8ScriptContext, global *v8.Object, cb func(error)) *eventLoop {
-	return &eventLoop{context, make(chan workItem), global, cb}
+	return &eventLoop{
+		clock.New(),
+		context,
+		make(chan workItem),
+		global,
+		cb,
+	}
 }
 
 type disposable interface {
@@ -50,42 +77,43 @@ type disposeFunc func()
 func (fn disposeFunc) dispose() { fn() }
 
 func (l *eventLoop) Start() disposable {
-	log.Debug("eventLoop.Start")
-	closer := make(chan bool)
-	go func() {
-		for i := range l.workItems {
-			func() {
-				defer l.ctx.endProcess()
-				if l.ctx.beginProcess() {
-					_, err := i.fn.Call(l.globalObject)
-					if err != nil {
-						fnAsString := i.fn.String()
-						go func() {
-							// Wrapped in go func() as it generates a deadlock on linux arm64 tests
-							log.Error(
-								"EventLoop: Error",
-								slog.String("script", fnAsString),
-								slog.String("error", err.Error()),
-								slog.String("stack", string(debug.Stack())),
-							)
-						}()
-						l.errorCb(err)
-					}
-				}
-			}()
-		}
-		closer <- true
-	}()
-	// There is some logic here that isn't tested specifically (but HTMX test
-	// fails if not implemented properly).
-	// When we shut down, we must be sure that we don't have any running scripts
-	// when disposing the v8 Isolate, otherwise that will cause a panic.
-	// That is why the close function waits for a channel event before proceeding
-	return disposeFunc(func() {
-		log.Debug("eventLoop.dispose")
-		close(l.workItems)
-		<-closer
-	})
+	return disposeFunc(func() {})
+	// log.Debug("eventLoop.Start")
+	// closer := make(chan bool)
+	// go func() {
+	// 	for i := range l.workItems {
+	// 		func() {
+	// 			defer l.ctx.endProcess()
+	// 			if l.ctx.beginProcess() {
+	// 				_, err := i.fn.Call(l.globalObject)
+	// 				if err != nil {
+	// 					fnAsString := i.fn.String()
+	// 					go func() {
+	// 						// Wrapped in go func() as it generates a deadlock on linux arm64 tests
+	// 						log.Error(
+	// 							"EventLoop: Error",
+	// 							slog.String("script", fnAsString),
+	// 							slog.String("error", err.Error()),
+	// 							slog.String("stack", string(debug.Stack())),
+	// 						)
+	// 					}()
+	// 					l.errorCb(err)
+	// 				}
+	// 			}
+	// 		}()
+	// 	}
+	// 	closer <- true
+	// }()
+	// // There is some logic here that isn't tested specifically (but HTMX test
+	// // fails if not implemented properly).
+	// // When we shut down, we must be sure that we don't have any running scripts
+	// // when disposing the v8 Isolate, otherwise that will cause a panic.
+	// // That is why the close function waits for a channel event before proceeding
+	// return disposeFunc(func() {
+	// 	log.Debug("eventLoop.dispose")
+	// 	close(l.workItems)
+	// 	<-closer
+	// })
 }
 
 func installEventLoopGlobals(host *V8ScriptHost, globalObjectTemplate *v8.ObjectTemplate) {
