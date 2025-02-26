dd21fb9507f0d637340839c30e5baf6588ae947c
Peter Strøiman <peter@stroiman.com>
2025-02-26 11:07:14 +0100

    Rewrite most element tests as testify suites

--- dom/element_test.go
+++ dom/element_test.go
@@ -1,182 +1,173 @@
 package dom_test
 
 import (
+	"testing"
+
+	"github.com/gost-dom/browser/dom"
 	. "github.com/gost-dom/browser/dom"
+	"github.com/gost-dom/browser/internal/test/scripttests"
+	. "github.com/gost-dom/browser/internal/testing/gomega-matchers"
 	. "github.com/gost-dom/browser/testing/gomega-matchers"
+	"github.com/stretchr/testify/suite"
 
 	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega"
 )
 
+type ElementTestSuite struct {
+	scripttests.GomegaSuite
+	doc dom.Document
+}
+
+func (s *ElementTestSuite) SetupTest() {
+	s.GomegaSuite.SetupTest()
+	s.doc = CreateHTMLDocument()
+}
+
+func (s *ElementTestSuite) TestGetSetAttribute() {
+	elm := s.doc.CreateElement("div")
+	s.Expect(elm.Attributes().Length()).To(Equal(0))
+	elm.SetAttribute("id", "1")
+	s.Expect(elm.Attributes().Length()).To(Equal(1))
+}
+
+func (s *ElementTestSuite) TestOverwriteExistingAttribute() {
+	elm := s.doc.CreateElement("div")
+	elm.SetAttribute("id", "1")
+	elm.SetAttribute("id", "2")
+	s.Expect(elm).To(HaveAttribute("id", "2"))
+	s.Expect(elm.Attributes().Length()).To(Equal(1))
+}
+
+func (s *ElementTestSuite) TestGetMissingAttribute() {
+	elm := s.doc.CreateElement("div")
+	_, ok := elm.GetAttribute("non-existing")
+	s.Expect(ok).To(BeFalse())
+}
+
+func (s *ElementTestSuite) TestGetMissingAttributeNode() {
+	elm := s.doc.CreateElement("div")
+	attr := elm.GetAttributeNode("class")
+	s.Expect(attr).To(BeNil())
+}
+
+func (s *ElementTestSuite) TestAttributeNodesAreMutable() {
+	elm := s.doc.CreateElement("div")
+	elm.SetAttribute("class", "foo")
+	attr := elm.GetAttributeNode("class")
+	s.Expect(attr).ToNot(BeNil())
+	s.Expect(attr.Value()).To(Equal("foo"), "Attribute value before mutation")
+	s.Expect(attr.Parent()).To(Equal(elm), "Parent on attribute node")
+	attr.SetValue("bar")
+	actual := elm.GetAttributeNode("class")
+	s.Expect(actual.Value()).To(Equal("bar"), "Attribute value after mutation")
+}
+
+func (s *ElementTestSuite) TestSetAttributeNodeAddsNew() {
+	elm := s.doc.CreateElement("div")
+	attr := s.doc.CreateAttribute("class")
+	attr.SetValue("foo")
+	result, err := elm.SetAttributeNode(attr)
+	s.Expect(err).ToNot(HaveOccurred())
+	s.Expect(result).To(BeNil())
+	s.Expect(elm.Attributes().Length()).To(Equal(1))
+	actual, _ := elm.GetAttribute("class")
+	s.Expect(actual).To(Equal("foo"))
+}
+
+func (s *ElementTestSuite) TestAttributeNodeUpdatesExisting() {
+	elm := s.doc.CreateElement("div")
+	elm.SetAttribute("class", "bar")
+	attr := s.doc.CreateAttribute("class")
+	attr.SetValue("foo")
+	result, err := elm.SetAttributeNode(attr)
+	s.Expect(err).ToNot(HaveOccurred())
+	s.Expect(result.Name()).To(Equal("class"))
+	s.Expect(result.Value()).To(Equal("bar"))
+	s.Expect(elm.Attributes().Length()).To(Equal(1))
+	actual, _ := elm.GetAttribute("class")
+	s.Expect(actual).To(Equal("foo"))
+}
+
+func (s *ElementTestSuite) TestReturnDOMErrorWhenMovingToWrongElement() {
+	elm := s.doc.CreateElement("div")
+	elm2 := s.doc.CreateElement("div")
+	elm2.SetAttribute("class", "bar")
+	attributeFromAnotherElement := elm2.GetAttributeNode("class")
+	s.Expect(
+		elm.SetAttributeNode(attributeFromAnotherElement),
+	).Error().To(BeADOMError())
+	s.Expect(elm.Attributes().Length()).To(Equal(0), "Target elm received attribute?")
+	s.Expect(
+		elm2.Attributes().Length(),
+	).To(Equal(1), "Target attribute still on source elm")
+}
+
+func (s *ElementTestSuite) TestRemoveExistingAttribute() {
+	elm := s.doc.CreateElement("div")
+	elm.SetAttribute("class", "bar")
+	nodeToRemove := elm.GetAttributeNode("class")
+	removedNode, err := elm.RemoveAttributeNode(nodeToRemove)
+	s.Expect(err).ToNot(HaveOccurred())
+	s.Expect(elm.Attributes().Length()).To(Equal(0))
+	s.Expect(removedNode).To(Equal(nodeToRemove))
+	s.Expect(removedNode.Parent()).To(BeNil(), "Attribute parent")
+}
+
+func (s *ElementTestSuite) TestRemoveNonExistingAttributeNode() {
+	elm := s.doc.CreateElement("div")
+	elm2 := s.doc.CreateElement("div")
+	elm2.SetAttribute("class", "bar")
+	attributeFromAnotherElement := elm2.GetAttributeNode("class")
+	s.Expect(
+		elm.RemoveAttributeNode(attributeFromAnotherElement),
+	).Error().To(BeADOMError())
+}
+
+func (s *ElementTestSuite) TestMatchesTagName() {
+	d := s.doc.CreateElement("div")
+	p := s.doc.CreateElement("p")
+	d.Append(p)
+	s.Assert().True(d.Matches("div"))
+	s.Assert().False(d.Matches("p"))
+	s.Assert().False(p.Matches("div"))
+}
+
+func (s *ElementTestSuite) TestMatchesAttribute() {
+	d := s.doc.CreateElement("div")
+	d.SetAttribute("known-attribute", "")
+	s.Assert().True(d.Matches("[known-attribute]"))
+	s.Assert().False(d.Matches("[unknown-attribute]"))
+}
+
+func (s *ElementTestSuite) TestMatchesAttributeNameValue() {
+	d := s.doc.CreateElement("div")
+	d.SetAttribute("a", "good")
+	s.Expect(d.Matches(`div[a="good"]`)).To(BeTrue())
+	s.Expect(d.Matches(`div[a="bad"]`)).To(BeFalse())
+}
+
+func (s *ElementTestSuite) TestTextContent() {
+	d := s.doc.CreateElement("div")
+	p := s.doc.CreateElement("p")
+	p.AppendChild(s.doc.CreateText("Original paragraph"))
+	d.AppendChild(p)
+	s.Expect(d).To(HaveTextContent("Original paragraph"))
+	d.SetTextContent("Replace the p")
+	s.Expect(d).To(HaveTextContent("Replace the p"))
+	s.Assert().Equal(1, d.ChildNodes().Length())
+	s.Assert().Equal(NodeTypeText, d.ChildNodes().Item(0).NodeType())
+}
+
+func (s *ElementTestSuite) TestSetTextContentRemovesChildren() {
+	s.T().Skip("Not implemented")
+}
+
+func TestElement(t *testing.T) {
+	suite.Run(t, new(ElementTestSuite))
+}
+
 var _ = Describe("Element", func() {
-	var doc Document
-
-	BeforeEach(func() {
-		doc = CreateHTMLDocument()
-		DeferCleanup(func() { doc = nil })
-	})
-
-	Describe("Get/Set attribute", func() {
-		It("Should add a new attribute when not existing", func() {
-			doc := CreateHTMLDocument()
-			elm := doc.CreateElement("div")
-			Expect(elm.Attributes().Length()).To(Equal(0))
-			elm.SetAttribute("id", "1")
-			Expect(elm.Attributes().Length()).To(Equal(1))
-		})
-
-		It("Should add overwrite an existing attribute", func() {
-			elm := doc.CreateElement("div")
-			elm.SetAttribute("id", "1")
-			elm.SetAttribute("id", "2")
-			Expect(elm).To(HaveAttribute("id", "2"))
-			Expect(elm.Attributes().Length()).To(Equal(1))
-		})
-
-		It("Should return nil when the attribute does't exist", func() {
-			elm := doc.CreateElement("div")
-			_, ok := elm.GetAttribute("non-existing")
-			Expect(ok).To(BeFalse())
-		})
-	})
-
-	Describe("Get/SetAttributeNode", func() {
-		It("getAttributeNode should return nil on missing name", func() {
-			elm := doc.CreateElement("div")
-			attr := elm.GetAttributeNode("class")
-			Expect(attr).To(BeNil())
-		})
-
-		It("getAttributeNode should return a mutable node on a valid name", func() {
-			elm := doc.CreateElement("div")
-			elm.SetAttribute("class", "foo")
-			attr := elm.GetAttributeNode("class")
-			Expect(attr).ToNot(BeNil())
-			Expect(attr.Value()).To(Equal("foo"), "Attribute value before mutation")
-			Expect(attr.Parent()).To(Equal(elm), "Parent on attribute node")
-			attr.SetValue("bar")
-			actual := elm.GetAttributeNode("class")
-			Expect(actual.Value()).To(Equal("bar"), "Attribute value after mutation")
-		})
-
-		Describe("SetAttribute", func() {
-			It("Should add an attribute if not existing", func() {
-				elm := doc.CreateElement("div")
-				attr := doc.CreateAttribute("class")
-				attr.SetValue("foo")
-				result, err := elm.SetAttributeNode(attr)
-				Expect(err).ToNot(HaveOccurred())
-				Expect(result).To(BeNil())
-				Expect(elm.Attributes().Length()).To(Equal(1))
-				actual, _ := elm.GetAttribute("class")
-				Expect(actual).To(Equal("foo"))
-			})
-
-			It("Should replace an attribute if it already exists", func() {
-				elm := doc.CreateElement("div")
-				elm.SetAttribute("class", "bar")
-				attr := doc.CreateAttribute("class")
-				attr.SetValue("foo")
-				result, err := elm.SetAttributeNode(attr)
-				Expect(err).ToNot(HaveOccurred())
-				Expect(result.Name()).To(Equal("class"))
-				Expect(result.Value()).To(Equal("bar"))
-				Expect(elm.Attributes().Length()).To(Equal(1))
-				actual, _ := elm.GetAttribute("class")
-				Expect(actual).To(Equal("foo"))
-			})
-
-			It("Should return a DOMError if the attribute belongs to another element", func() {
-				elm := doc.CreateElement("div")
-				elm2 := doc.CreateElement("div")
-				elm2.SetAttribute("class", "bar")
-				attributeFromAnotherElement := elm2.GetAttributeNode("class")
-				Expect(
-					elm.SetAttributeNode(attributeFromAnotherElement),
-				).Error().To(BeADOMError())
-				Expect(elm.Attributes().Length()).To(Equal(0), "Target elm received attribute?")
-				Expect(
-					elm2.Attributes().Length(),
-				).To(Equal(1), "Target attribute still on source elm")
-			})
-		})
-
-		Describe("RemoveAttribute", func() {
-			It("Should remove the attribute if it is on the element", func() {
-				elm := doc.CreateElement("div")
-				elm.SetAttribute("class", "bar")
-				nodeToRemove := elm.GetAttributeNode("class")
-				removedNode, err := elm.RemoveAttributeNode(nodeToRemove)
-				Expect(err).ToNot(HaveOccurred())
-				Expect(elm.Attributes().Length()).To(Equal(0))
-				Expect(removedNode).To(Equal(nodeToRemove))
-				Expect(removedNode.Parent()).To(BeNil(), "Attribute parent")
-			})
-
-			It("Should return a DOMError when attribute doesn't exist on the node", func() {
-				elm := doc.CreateElement("div")
-				elm2 := doc.CreateElement("div")
-				elm2.SetAttribute("class", "bar")
-				attributeFromAnotherElement := elm2.GetAttributeNode("class")
-				Expect(
-					elm.RemoveAttributeNode(attributeFromAnotherElement),
-				).Error().To(BeADOMError())
-			})
-		})
-	})
-
-	Describe("Matches", func() {
-		It("Should return true for a simple string matching the root element", func() {
-			d := doc.CreateElement("div")
-			p := doc.CreateElement("p")
-			d.Append(p)
-			Expect(d.Matches("div")).To(BeTrue())
-		})
-
-		It("Should return false for a simple string matching a child element", func() {
-			d := doc.CreateElement("div")
-			p := doc.CreateElement("p")
-			d.Append(p)
-			Expect(d.Matches("p")).To(BeFalse())
-		})
-
-		It("Should return true for an existing attribute", func() {
-			d := doc.CreateElement("div")
-			d.SetAttribute("known-attribute", "")
-			Expect(d.Matches("[known-attribute]")).To(BeTrue())
-		})
-
-		It("Should return true if one attribute match", func() {
-			d := doc.CreateElement("div")
-			d.SetAttribute("known-attribute", "")
-			Expect(d.Matches("[unknown-attribute], [known-attribute]")).To(BeTrue())
-		})
-
-		It("Should return false for a non-existing attribute", func() {
-			d := doc.CreateElement("div")
-			Expect(d.Matches("[unknown-attribute]")).To(BeFalse())
-		})
-
-		It("Should return true if tagname + attribute key=value has right value", func() {
-			d := doc.CreateElement("div")
-			d.SetAttribute("a", "right")
-			Expect(d.Matches(`div[a="right"]`)).To(BeTrue())
-		})
-
-		It("Should return true if tagname + attribute key=value has wrong value", func() {
-			d := doc.CreateElement("div")
-			d.SetAttribute("a", "right")
-			Expect(d.Matches(`div[a="wrong"]`)).To(BeFalse())
-		})
-	})
-
-	It("Should support Get/SetTextContent", func() {
-		d := doc.CreateElement("div")
-		d.AppendChild(doc.CreateElement("p"))
-		d.SetTextContent("Replace the p")
-		Expect(d).To(HaveTextContent("Replace the p"))
-	})
-
 	Describe("InsertAdjacentHTML", func() {
 		It("Should insert correctly 'beforeBegin'", func() {
 			doc := ParseHtmlString(`<body>
@@ -188,11 +179,11 @@ var _ = Describe("Element", func() {
   <div id="3">El 1</div>
 </body>`)
 			el := doc.GetElementById("2")
-			Expect(el.InsertAdjacentHTML(
+			gomega.Expect(el.InsertAdjacentHTML(
 				"beforebegin",
 				"<div>1st new child</div><div>2nd new child</div>",
 			)).To(Succeed())
-			Expect(doc.Body()).To(HaveOuterHTML(`<body>
+			gomega.Expect(doc.Body()).To(HaveOuterHTML(`<body>
   <div id="1">El 1</div>
   <div>1st new child</div><div>2nd new child</div><div id="2">El 2
     <div>El 2-a</div>
@@ -213,14 +204,14 @@ var _ = Describe("Element", func() {
   <div id="3">El 1</div>
 </body>`)
 			el, err := (doc.QuerySelector("[id='2']"))
-			Expect(err).ToNot(HaveOccurred())
-			Expect(
+			gomega.Expect(err).ToNot(HaveOccurred())
+			gomega.Expect(
 				el.InsertAdjacentHTML(
 					"afterbegin",
 					"<div>1st new child</div><div>2nd new child</div>",
 				),
 			).To(Succeed())
-			Expect(doc.Body()).To(HaveOuterHTML(`<body>
+			gomega.Expect(doc.Body()).To(HaveOuterHTML(`<body>
   <div id="1">El 1</div>
   <div id="2"><div>1st new child</div><div>2nd new child</div>El 2
     <div>El 2-a</div>
@@ -241,14 +232,14 @@ var _ = Describe("Element", func() {
   <div id="3">El 1</div>
 </body>`)
 			el, err := (doc.QuerySelector("[id='2']"))
-			Expect(err).ToNot(HaveOccurred())
-			Expect(
+			gomega.Expect(err).ToNot(HaveOccurred())
+			gomega.Expect(
 				el.InsertAdjacentHTML(
 					"beforeend",
 					"<div>1st new child</div><div>2nd new child</div>",
 				),
 			).To(Succeed())
-			Expect(doc.Body()).To(HaveOuterHTML(`<body>
+			gomega.Expect(doc.Body()).To(HaveOuterHTML(`<body>
   <div id="1">El 1</div>
   <div id="2">El 2
     <div>El 2-a</div>
@@ -269,14 +260,14 @@ var _ = Describe("Element", func() {
   <div id="3">El 1</div>
 </body>`)
 			el, err := (doc.QuerySelector("[id='2']"))
-			Expect(err).ToNot(HaveOccurred())
-			Expect(
+			gomega.Expect(err).ToNot(HaveOccurred())
+			gomega.Expect(
 				el.InsertAdjacentHTML(
 					"afterend",
 					"<div>1st new child</div><div>2nd new child</div>",
 				),
 			).To(Succeed())
-			Expect(doc.Body()).To(HaveOuterHTML(`<body>
+			gomega.Expect(doc.Body()).To(HaveOuterHTML(`<body>
   <div id="1">El 1</div>
   <div id="2">El 2
     <div>El 2-a</div>
@@ -293,11 +284,11 @@ var _ = Describe("Element", func() {
     <div>El 2-a</div>
     <div>El 2-b</div>
   </div></body>`)
-			Expect(doc.Body().OuterHTML()).To(Equal(`<body><div id="2">El 2
+			gomega.Expect(doc.Body().OuterHTML()).To(Equal(`<body><div id="2">El 2
     <div>El 2-a</div>
     <div>El 2-b</div>
   </div></body>`))
-			Expect(doc.Body().InnerHTML()).To(Equal(`<div id="2">El 2
+			gomega.Expect(doc.Body().InnerHTML()).To(Equal(`<div id="2">El 2
     <div>El 2-a</div>
     <div>El 2-b</div>
   </div>`))
@@ -313,8 +304,8 @@ var _ = Describe("Element", func() {
 				event = e
 			}))
 			element.Click()
-			Expect(event.Cancelable()).To(BeTrue())
-			Expect(event.Bubbles()).To(BeTrue())
+			gomega.Expect(event.Cancelable()).To(BeTrue())
+			gomega.Expect(event.Bubbles()).To(BeTrue())
 		})
 	})
 
@@ -333,7 +324,7 @@ var _ = Describe("Element", func() {
 				divE,
 				doc.CreateText("f"),
 			)
-			Expect(b).To(HaveOuterHTML(`<body>a<div>b</div>cd<div>e</div>f</body>`))
+			gomega.Expect(b).To(HaveOuterHTML(`<body>a<div>b</div>cd<div>e</div>f</body>`))
 		})
 
 		It("Should add elements first in 'Prepend'", func() {
@@ -346,7 +337,7 @@ var _ = Describe("Element", func() {
 				divE,
 				doc.CreateText("f"),
 			)
-			Expect(b).To(HaveOuterHTML(`<body>d<div>e</div>fa<div>b</div>c</body>`))
+			gomega.Expect(b).To(HaveOuterHTML(`<body>d<div>e</div>fa<div>b</div>c</body>`))
 		})
 
 		It("Should replace elements first in 'ReplaceChildren'", func() {
@@ -359,7 +350,7 @@ var _ = Describe("Element", func() {
 				divE,
 				doc.CreateText("f"),
 			)
-			Expect(b).To(HaveOuterHTML(`<body>d<div>e</div>f</body>`))
+			gomega.Expect(b).To(HaveOuterHTML(`<body>d<div>e</div>f</body>`))
 		})
 
 		It("Should iterate elements in 'Children'", func() {
@@ -369,17 +360,17 @@ var _ = Describe("Element", func() {
 			b := doc.Body()
 			c := b.Children()
 
-			Expect(c.Length()).To(Equal(3))
-			Expect(c.Item(0)).To(HaveOuterHTML(`<div id="el-1">b</div>`))
-			Expect(c.Item(1)).To(HaveAttribute("id", "el-2"))
-			Expect(c.Item(2)).To(HaveAttribute("name", "el-3"))
+			gomega.Expect(c.Length()).To(Equal(3))
+			gomega.Expect(c.Item(0)).To(HaveOuterHTML(`<div id="el-1">b</div>`))
+			gomega.Expect(c.Item(1)).To(HaveAttribute("id", "el-2"))
+			gomega.Expect(c.Item(2)).To(HaveAttribute("name", "el-3"))
 
-			Expect(c.Item(-1)).To(BeNil())
-			Expect(c.Item(3)).To(BeNil())
+			gomega.Expect(c.Item(-1)).To(BeNil())
+			gomega.Expect(c.Item(3)).To(BeNil())
 
-			Expect(c.NamedItem("el-1")).To(HaveAttribute("id", "el-1"))
-			Expect(c.NamedItem("el-2")).To(HaveAttribute("id", "el-2"))
-			Expect(c.NamedItem("el-3")).To(HaveAttribute("name", "el-3"))
+			gomega.Expect(c.NamedItem("el-1")).To(HaveAttribute("id", "el-1"))
+			gomega.Expect(c.NamedItem("el-2")).To(HaveAttribute("id", "el-2"))
+			gomega.Expect(c.NamedItem("el-3")).To(HaveAttribute("name", "el-3"))
 		})
 
 		Describe("First/Last element", func() {
@@ -388,8 +379,8 @@ var _ = Describe("Element", func() {
 					`<body>a<div id="el-1">b</div>c<div id="el-2">d</div>e<div name="el-3">f</div>g</body>`,
 				)
 				b := doc.Body()
-				Expect(b.FirstElementChild()).To(HaveAttribute("id", "el-1"))
-				Expect(b.LastElementChild()).To(HaveOuterHTML(`<div name="el-3">f</div>`))
+				gomega.Expect(b.FirstElementChild()).To(HaveAttribute("id", "el-1"))
+				gomega.Expect(b.LastElementChild()).To(HaveOuterHTML(`<div name="el-3">f</div>`))
 			})
 
 			It("Should nil when there are only non-element children", func() {
@@ -397,8 +388,8 @@ var _ = Describe("Element", func() {
 					`<body>body text</body>`,
 				)
 				b := doc.Body()
-				Expect(b.FirstElementChild()).To(BeNil())
-				Expect(b.LastElementChild()).To(BeNil())
+				gomega.Expect(b.FirstElementChild()).To(BeNil())
+				gomega.Expect(b.LastElementChild()).To(BeNil())
 			})
 		})
 
