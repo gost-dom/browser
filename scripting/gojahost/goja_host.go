dbc371b1ba48c7839282e1a166b9d50bfa737b70
Peter Strøiman <peter@stroiman.com>
2025-02-27 13:04:03 +0100

    refactor(goja): Restructure goja script host
    Makes the file names consistent with the terminology used in the
    project, "Script host" and "Script context".

--- scripting/gojahost/goja_host.go
+++ scripting/gojahost/goja_host.go
@@ -1,7 +1,6 @@
 package gojahost
 
 import (
-	"errors"
 	"fmt"
 	"reflect"
 	"strings"
@@ -173,95 +172,4 @@ func (d *gojaScriptHost) NewContext(window html.Window) html.ScriptContext {
 	return result
 }
 
-func (m *GojaContext) createLocationInstance() *goja.Object {
-	location := m.vm.CreateObject(m.globals["Location"].Prototype)
-	location.DefineDataPropertySymbol(
-		m.wrappedGoObj,
-		m.vm.ToValue(m.window.Location()),
-		goja.FLAG_FALSE,
-		goja.FLAG_FALSE,
-		goja.FLAG_FALSE,
-	)
-	return location
-}
 func (d *gojaScriptHost) Close() {}
-
-type GojaContext struct {
-	vm           *goja.Runtime
-	clock        *clock.Clock
-	window       html.Window
-	globals      map[string]function
-	wrappedGoObj *goja.Symbol
-	cachedNodes  map[int32]goja.Value
-}
-
-func (c *GojaContext) Clock() html.Clock { return c.clock }
-
-func (i *GojaContext) Close() {}
-
-func (i *GojaContext) run(str string) (goja.Value, error) {
-	res, err := i.vm.RunString(str)
-	i.clock.Tick()
-	return res, err
-}
-
-func (i *GojaContext) Run(str string) error {
-	_, err := i.run(str)
-	return err
-}
-
-func (i *GojaContext) Eval(str string) (res any, err error) {
-	if gojaVal, err := i.run(str); err == nil {
-		return gojaVal.Export(), nil
-	} else {
-		return nil, err
-	}
-}
-
-func (i *GojaContext) EvalCore(str string) (res any, err error) {
-	return i.vm.RunString(str)
-}
-
-func (i *GojaContext) RunFunction(str string, arguments ...any) (res any, err error) {
-	var f goja.Value
-	if f, err = i.vm.RunString(str); err == nil {
-		if c, ok := goja.AssertFunction(f); !ok {
-			err = errors.New("GojaContext.RunFunction: script is not a function")
-		} else {
-			values := make([]goja.Value, len(arguments))
-			for i, a := range arguments {
-				var ok bool
-				if values[i], ok = a.(goja.Value); !ok {
-					err = fmt.Errorf("GojaContext.RunFunction: argument %d was not a goja Value", i)
-				}
-			}
-			res, err = c(goja.Undefined(), values...)
-		}
-	}
-	return
-}
-
-// Export create a native Go value out of a javascript value. The value argument
-// must be a [goja.Value] instance.
-//
-// This function is intended to be used only for test purposes. The value has an
-// [any] type as the tests are not supposed to know the details of the
-// underlying engine.
-//
-// The value is expected to be the ourput of [RunFunction] or [EvalCore]
-//
-// An error will be returned if the value is not a goja Value, or the value
-// could not be converted to a native Go object
-func (i *GojaContext) Export(value any) (res any, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = fmt.Errorf("GojaContext.Export: %v", r)
-		}
-	}()
-	if gv, ok := value.(goja.Value); ok {
-		res = gv.Export()
-	} else {
-		err = fmt.Errorf("GojaContext.Export: Value not a goja value: %v", value)
-	}
-	return
-}
