61391a8c5921ee76e5abf9814a4b68f90614a531
Peter Strøiman <peter@stroiman.com>
2025-02-27 21:41:36 +0100

    Extract script_host_suite

--- internal/test/scripttests/location_suite.go
+++ internal/test/scripttests/location_suite.go
@@ -2,67 +2,8 @@ package scripttests
 
 import (
 	"github.com/gost-dom/browser/html"
-	"github.com/onsi/gomega"
 	. "github.com/onsi/gomega"
-	"github.com/stretchr/testify/suite"
 )
-
-type GomegaSuite struct {
-	suite.Suite
-	gomega.Gomega
-}
-
-func (s *GomegaSuite) SetupTest() {
-	s.Gomega = gomega.NewWithT(s.T())
-}
-
-type ScriptHostSuite struct {
-	GomegaSuite
-	scriptHost html.ScriptHost
-	window     html.Window
-}
-
-func (s *ScriptHostSuite) SetupTest() {
-	s.GomegaSuite.SetupTest()
-	s.window = html.NewWindow(html.WindowOptions{
-		ScriptHost: s.scriptHost,
-	})
-}
-
-func (s *ScriptHostSuite) OpenWindow(location string) html.Window {
-	err := s.window.Navigate(location)
-	s.Assert().NoError(err)
-	return s.window
-}
-
-func (s *ScriptHostSuite) TeardownTest() {
-	s.window.Close()
-}
-
-// Runs a script and returns the evaluated value as a native Go value.
-//
-// Panics (or generates an error?) if no suitable conversion could be found (i.e.
-// gost doesn't implement this yet).
-//
-// Returns an error if script code throws.
-//
-// If the return value is not used, call run; to avoid panic/error
-func (s *ScriptHostSuite) eval(script string) (any, error) {
-	return s.window.Eval(script)
-}
-
-// Runs a script, and discards the returned value.
-//
-// Returns an error if the script code throws.
-func (s *ScriptHostSuite) run(script string) error {
-	return s.window.Run(script)
-}
-
-func NewScriptHostSuite(h html.ScriptHost) *ScriptHostSuite {
-	return &ScriptHostSuite{
-		scriptHost: h,
-	}
-}
 
 type LocationSuite struct {
 	ScriptHostSuite
