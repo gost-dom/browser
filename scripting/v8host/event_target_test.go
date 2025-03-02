b137c0362b34b8d5a8bf1627252d4433806a90f5
Peter Strøiman <peter@stroiman.com>
2025-03-02 22:41:39 +0100

    Refactor test to std go test

--- scripting/v8host/event_target_test.go
+++ scripting/v8host/event_target_test.go
@@ -1,27 +1,28 @@
 package v8host_test
 
 import (
-	. "github.com/onsi/ginkgo/v2"
+	"testing"
+
+	"github.com/onsi/gomega"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("V8 EventTarget", func() {
-	It("Should be able to add and remove event listeners", func() {
-		win := scriptTestSuite.NewWindow()
-		Expect(win.Eval(`
-			let events = [];
-			let noOfEvents = []
-			const handler = e => { events.push(e) }
-			window.addEventListener("gost", handler)
-			window.dispatchEvent(new CustomEvent("gost"))
-			noOfEvents.push(events.length)
-			window.removeEventListener("gost", handler)
-			window.dispatchEvent(new CustomEvent("gost"))
-			noOfEvents.push(events.length)
-			window.addEventListener("gost", handler)
-			window.dispatchEvent(new CustomEvent("gost"))
-			noOfEvents.push(events.length)
-			noOfEvents
-		`)).To(HaveExactElements([]int32{1, 1, 2}))
-	})
-})
+func TestV8EventTargetAddRemoveListeners(t *testing.T) {
+	g := gomega.NewWithT(t)
+	win := scriptTestSuite.NewWindow()
+	g.Expect(win.Eval(`
+		let events = [];
+		let noOfEvents = []
+		const handler = e => { events.push(e) }
+		window.addEventListener("gost", handler)
+		window.dispatchEvent(new CustomEvent("gost"))
+		noOfEvents.push(events.length)
+		window.removeEventListener("gost", handler)
+		window.dispatchEvent(new CustomEvent("gost"))
+		noOfEvents.push(events.length)
+		window.addEventListener("gost", handler)
+		window.dispatchEvent(new CustomEvent("gost"))
+		noOfEvents.push(events.length)
+		noOfEvents
+	`)).To(HaveExactElements([]int32{1, 1, 2}))
+}
