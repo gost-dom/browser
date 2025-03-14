bbc9bd5d8996fe017002b4f9281501d851c8141c
Peter Strøiman <peter@stroiman.com>
2025-03-14 18:04:54 +0100

    test: Delete skipped tests
    It doesn't appear that this will be applicable any time soon

--- internal/code-gen/html-elements/html_generators_test.go
+++ internal/code-gen/html-elements/html_generators_test.go
@@ -3,7 +3,6 @@ package htmlelements_test
 import (
 	"testing"
 
-	. "github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 	. "github.com/onsi/gomega"
 
@@ -15,12 +14,12 @@ func GenerateHtmlAnchor() (g.Generator, error) {
 	return getIdlInterfaceGenerator("html", "HTMLAnchorElement")
 }
 
-func TestHTMLAnchor(t *testing.T) {
+func TestHTMLAnchorInterface(t *testing.T) {
 	Expect := gomega.NewWithT(t).Expect
 
 	Expect(GenerateHtmlAnchor()).To(HaveRendered(MatchRegexp(
 		`type HTMLAnchorElement interface {\n\tHTMLElement`)),
-		"Rendered HTMLAnchor interface declaration")
+		"Rendered HTMLAnchorElement interface declaration")
 
 	Expect(GenerateHtmlAnchor()).To(HaveRendered(
 		MatchRegexp("(?m)^\tHTMLHyperlinkElementUtils$")),
@@ -38,59 +37,4 @@ func TestHTMLAnchor(t *testing.T) {
 		GenerateHtmlAnchor(),
 	).ToNot(HaveRendered(MatchRegexp(`func \([^(]+\) Host\(\) string`)),
 		"Rendered implementation for URL properties")
-
 }
-
-var _ = Describe("ElementGenerator", func() {
-	It("Should generate a getter and setter", func() {
-		Skip("Atrribute implementation currently disabled because of RelList()")
-		Expect(GenerateHtmlAnchor()).To(HaveRendered(ContainSubstring(
-			`func (e *htmlAnchorElement) Target() string {`)))
-	})
-
-	It("Should NOT sanitize type", func() {
-		Skip("Atrribute implementation currently disabled because of RelList()")
-		Expect(GenerateHtmlAnchor()).To(HaveRendered(ContainSubstring(
-			`func (e *htmlAnchorElement) Type() string`)))
-	})
-
-	It("Should generate a struct with embedded htmlElement", func() {
-		Skip("Disabled for HTMLAnchorElement, custom data in struct")
-		Expect(GenerateHtmlAnchor()).To(HaveRendered(MatchRegexp(
-			`type htmlAnchorElement struct {\n\tHTMLElement`)))
-	})
-
-	It("Should generate an interface ", func() {
-	})
-
-	It("Should generate a constructor", func() {
-		Skip("Disabled for HTMLAnchorElement, custom construction")
-		Expect(GenerateHtmlAnchor()).To(HaveRendered(ContainSubstring(
-			`func NewHTMLAnchorElement(ownerDoc HTMLDocument) HTMLAnchorElement {
-	result := &htmlAnchorElement{NewHTMLElement("a", ownerDoc)}
-	result.SetSelf(result)
-	return result
-}`)))
-	})
-
-	Describe("HTMLAnchorElement", func() {
-		It("Should have embedded HyperLinkUtils", func() {
-			Expect(GenerateHtmlAnchor()).To(HaveRendered(
-				MatchRegexp("(?m)^\tHTMLHyperlinkElementUtils$")))
-
-		})
-		It("Should have interface for URL properties", func() {
-			Expect(GenerateHtmlAnchor()).To(HaveRendered(ContainSubstring("\n\tTarget() string\n")))
-		})
-
-		It("Should not setters for read-only properties", func() {
-			Expect(GenerateHtmlAnchor()).ToNot(HaveRendered(ContainSubstring("\tSetOrigin(")))
-		})
-
-		It("Should not have an implementation for URL properties", func() {
-			Expect(
-				GenerateHtmlAnchor(),
-			).ToNot(HaveRendered(MatchRegexp(`func \([^(]+\) Host\(\) string`)))
-		})
-	})
-})
