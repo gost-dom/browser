b9231ffdbf66f2d162fd5f58428f66c1eb47bf01
Peter Strøiman <peter@stroiman.com>
2025-03-17 09:44:20 +0100

    docs: Improve package docs for package interfaces

--- internal/interfaces/package.go
+++ internal/interfaces/package.go
@@ -1,7 +1,14 @@
-// Package interfaces is an internal package containing generated go interfaces
+// Package interfaces contains go interfaces generated from IDL specs
 //
-// These exist as a "guarantee" that types implementing a specific IDL interface
-// has the correct methods. Some of these may be used in actual code, e.g.,
-// javascript wrapper classes; and some may not even be used in real code; but
-// with a test in this package to ensure that a type conforms to the interface.
+// The generated interfaces guarantee that go interfaces correspond to IDL
+// specifications. Not all interfaces are used in production code; but then they
+// are used in test code, where a type assertion is used to guarantee that a go
+// type corresponds to a concrete interface.
+//
+// This serves as a verification that web APIs are implemented according to the
+// interfaces, as a test will fail if not; and can detect changes to the web IDL
+// standards.
+//
+// The types are also kept in a separate package for that exact reason, to avoid
+// polluting run-time packages with code that has not use at run-time.
 package interfaces
