861d0b3fbba412f9298b5eca6d8925c62f451a64
Peter Strøiman <peter@stroiman.com>
2025-03-20 13:40:57 +0100

    test: Convert ginkgo test to naked test

--- scripting/v8host/script_host_test.go
+++ scripting/v8host/script_host_test.go
@@ -2,31 +2,28 @@ package v8host_test
 
 import (
 	"strings"
+	"testing"
 
 	"github.com/gost-dom/browser/html"
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	. "github.com/gost-dom/browser/internal/testing/gomega-matchers"
+	"github.com/onsi/gomega"
 )
 
-var _ = Describe("ScriptHost", func() {
-	Describe("Script host", func() {
-		Describe("Load document with script", func() {
-			It("Runs the script when connected to DOM", func() {
-				reader := strings.NewReader(`<html><body>
+func TestScriptHostDocumentScriptLoading(t *testing.T) {
+	Expect := gomega.NewWithT(t).Expect
+	reader := strings.NewReader(`<html><body>
     <script>window.sut = document.documentElement.outerHTML</script>
     <div>I should not be in the output</div>
   </body></html>
 `)
-				options := html.WindowOptions{ScriptHost: host}
-				win, err := html.NewWindowReader(reader, options)
-				defer win.Close()
-				Expect(err).ToNot(HaveOccurred())
-				ctx := win.ScriptContext()
-				Expect(
-					ctx.Eval("window.sut"),
-				).To(Equal(`<html><head></head><body>
+	options := html.WindowOptions{ScriptHost: host}
+	win, err := html.NewWindowReader(reader, options)
+	defer win.Close()
+	Expect(err).ToNot(HaveOccurred())
+	ctx := win.ScriptContext()
+	Expect(
+		ctx.Eval("window.sut"),
+	).To(Equal(`<html><head></head><body>
     <script>window.sut = document.documentElement.outerHTML</script></body></html>`))
-			})
-		})
-	})
-})
+
+}
