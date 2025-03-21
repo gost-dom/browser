a5b947b4d6b8eb1056f2a0b0b2a6aee51bd0fd58
Peter Strøiman <peter@stroiman.com>
2025-03-21 14:47:46 +0100

    work: Handle 301 redirects from user agent

--- html/window.go
+++ html/window.go
@@ -108,6 +108,14 @@ func newWindow(windowOptions ...WindowOption) *window {
 	return win
 }
 
+func (w *window) checkRedirect(req *http.Request, via []*http.Request) error {
+	if len(via) > 9 {
+		return errors.New("Too many redirects")
+	}
+	w.history.ReplaceState(EMPTY_STATE, req.URL.String())
+	return nil
+}
+
 func NewWindow(windowOptions ...WindowOption) Window {
 	return newWindow(windowOptions...)
 }
@@ -209,8 +217,13 @@ func (w *window) Document() dom.Document {
 	return w.document
 }
 
-func (w *window) Navigate(href string) error {
+func (w *window) Navigate(href string) (err error) {
 	log.Info(w.Logger(), "Window.navigate:", "href", href)
+	defer func() {
+		if err != nil {
+			log.Warn(w.logger, "Window.navigate: Error response", "err", err.Error())
+		}
+	}()
 	w.History().pushLoad(href)
 	w.initScriptEngine()
 	w.baseLocation = href
@@ -236,15 +249,23 @@ func (w *window) reload(href string) error {
 	}
 }
 
-func (w *window) get(href string) (err error) {
+func (w *window) get(href string) error {
 	if req, err := http.NewRequest("GET", href, nil); err == nil {
-		err = w.fetchRequest(req)
+		return w.fetchRequest(req)
+	} else {
+		return err
 	}
-	return err
 }
 
+// fetchRequest handles "User agent" requests. I.e., navigation by clicking a
+// link, submitting a form, of calling a JS API that makes the browser itself
+// navigate to a new URL.
 func (w *window) fetchRequest(req *http.Request) error {
-	resp, err := w.httpClient.Do(req)
+	// Create a copy of the client, and set the CheckRedirect to a function that
+	// updates the window location to reflect the new URL.
+	client := w.httpClient
+	client.CheckRedirect = w.checkRedirect
+	resp, err := client.Do(req)
 	if err != nil {
 		return err
 	}
