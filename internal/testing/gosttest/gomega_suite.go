46a430f3adccef05493ebacee19717f4b0b152da
Peter Strøiman <peter@stroiman.com>
2025-03-21 13:07:46 +0100

    docs: Improve internal testing tool docs

--- internal/testing/gosttest/gomega_suite.go
+++ internal/testing/gosttest/gomega_suite.go
@@ -6,11 +6,17 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
+// GomegaSuite is a specialised [suite.Suite] that provides support for the
+// [gomega] assertion library by providing a [GomegaSuite.Expect] function
+// automatically initializes gomega with
 type GomegaSuite struct {
 	suite.Suite
 	gomega gomega.Gomega
 }
 
+// Expect is wraps [gomega/Gomega.Expect], passing the correct [testing/T]. This
+// supports gomega matchers for verification; which can in some cases provide
+// more expressive tests.
 func (s *GomegaSuite) Expect(actual interface{}, extra ...interface{}) types.Assertion {
 	if s.gomega == nil {
 		s.gomega = gomega.NewWithT(s.T())
