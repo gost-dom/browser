3bd7f0f5018d42e415cb3615c03381b2c060158d
Peter Strøiman <peter@stroiman.com>
2025-03-25 11:07:54 +0100

    test: Restructure test file

--- internal/dom/mutation/observer_test.go
+++ internal/dom/mutation/observer_test.go
@@ -28,6 +28,106 @@ type MutationObserverTestSuite struct {
 	suite.Suite
 }
 
+func (s *MutationObserverTestSuite) TestObserveChildListNoSubtree() {
+	var recorder MutationRecorder
+	observer := NewObserver(&recorder, &recorder)
+
+	doc := html.NewHTMLDocument(nil)
+	body := doc.Body()
+
+	observer.Observe(body, ChildList)
+
+	div := doc.CreateElement("div")
+	div.SetAttribute("id", "parent")
+	body.AppendChild(div)
+
+	recorder.Flush()
+	s.Assert().Equal([]dom.Node{body}, recorder.Targets())
+	s.Assert().Equal([]dom.Node{div}, recorder.AddedNodes())
+	s.Assert().Empty(recorder.RemovedNodes())
+	recorder.Clear()
+
+	div.AppendChild(doc.CreateElement("div"))
+	recorder.Flush()
+	s.Assert().Empty(recorder.Records, "Child node was notified when subtree=false")
+
+	body.RemoveChild(div)
+	recorder.Flush()
+	s.Assert().Equal([]dom.Node{body}, recorder.Targets())
+	g := gomega.NewWithT(s.T())
+	g.Expect(recorder.RemovedNodes()).To(gomega.ConsistOf(div))
+	s.Assert().Empty(recorder.AddedNodes())
+}
+
+func (s *MutationObserverTestSuite) TestObserveChildListWithSubtree() {
+	recorder := &MutationRecorder{}
+	observer := NewObserver(recorder, recorder)
+
+	doc := html.NewHTMLDocument(nil)
+	body := doc.Body()
+
+	div := doc.CreateElement("div")
+	body.AppendChild(div)
+	observer.Observe(body, ChildList, Subtree)
+
+	child := doc.CreateElement("div")
+	div.AppendChild(child)
+
+	recorder.Flush()
+	s.Assert().Equal([]dom.Node{div}, recorder.Targets())
+	s.Assert().Equal([]dom.Node{child}, recorder.AddedNodes())
+}
+
+func (s *MutationObserverTestSuite) TestValidOptions() {
+	assert := s.Assert()
+	var rec = new(MutationRecorder)
+
+	doc := html.NewHTMLDocument(nil)
+
+	assert.NoError(NewObserver(rec, rec).Observe(doc, ChildList), "Error when including ChildList")
+	assert.NoError(
+		NewObserver(rec, rec).Observe(doc, Attributes),
+		"Error when including Attributes",
+	)
+	assert.NoError(NewObserver(rec, rec).Observe(doc, CharacterData),
+		"Error when including CharacterData")
+
+	observer := NewObserver(rec, rec)
+	assert.ErrorIs(observer.Observe(doc), gosterror.TypeError{},
+		"Error when calling with no arguments")
+	assert.ErrorIs(
+		NewObserver(rec, rec).Observe(doc,
+			Subtree,
+			AttributeOldValue,
+			CharacterDataOldValue,
+			AttributeFilter([]string{"dummy"})),
+		gosterror.TypeError{},
+		"Error when _all_ non other options than one of the 3 required is passed",
+	)
+}
+
+func Test(t *testing.T) {
+	suite.Run(t, new(MutationObserverTestSuite))
+}
+
+/*
+TODO: Note this about subtree monitoring:
+
+subtree (optional)
+
+Set to true to extend monitoring to the entire subtree of nodes rooted at
+target. All of the other properties are then extended to all of the nodes in the
+subtree instead of applying solely to the target node. The default value is
+false.
+
+**Note that if a descendant of target is removed, changes in that
+descendant subtree will continue to be observed, until the notification about
+the removal itself has been delivered.**
+
+source: https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver/observe
+
+*/
+
 type MutationRecorder struct {
 	flushers map[mutation.Flusher]struct{}
 	Records  []Record
@@ -93,103 +193,3 @@ func (r MutationRecorder) RemovedNodes() []dom.Node {
 	}
 	return slices.Concat(lists...)
 }
-
-func (s *MutationObserverTestSuite) TestObserveChildListNoSubtree() {
-	var recorder MutationRecorder
-	observer := NewObserver(&recorder, &recorder)
-
-	doc := html.NewHTMLDocument(nil)
-	body := doc.Body()
-
-	observer.Observe(body, ChildList)
-
-	div := doc.CreateElement("div")
-	div.SetAttribute("id", "parent")
-	body.AppendChild(div)
-
-	recorder.Flush()
-	s.Assert().Equal([]dom.Node{body}, recorder.Targets())
-	s.Assert().Equal([]dom.Node{div}, recorder.AddedNodes())
-	s.Assert().Empty(recorder.RemovedNodes())
-	recorder.Clear()
-
-	div.AppendChild(doc.CreateElement("div"))
-	recorder.Flush()
-	s.Assert().Empty(recorder.Records, "Child node was notified when subtree=false")
-
-	body.RemoveChild(div)
-	recorder.Flush()
-	s.Assert().Equal([]dom.Node{body}, recorder.Targets())
-	g := gomega.NewWithT(s.T())
-	g.Expect(recorder.RemovedNodes()).To(gomega.ConsistOf(div))
-	s.Assert().Empty(recorder.AddedNodes())
-}
-
-func (s *MutationObserverTestSuite) TestObserveChildListWithSubtree() {
-	recorder := &MutationRecorder{}
-	observer := NewObserver(recorder, recorder)
-
-	doc := html.NewHTMLDocument(nil)
-	body := doc.Body()
-
-	div := doc.CreateElement("div")
-	body.AppendChild(div)
-	observer.Observe(body, ChildList, Subtree)
-
-	child := doc.CreateElement("div")
-	div.AppendChild(child)
-
-	recorder.Flush()
-	s.Assert().Equal([]dom.Node{div}, recorder.Targets())
-	s.Assert().Equal([]dom.Node{child}, recorder.AddedNodes())
-}
-
-func TestValidOptions(t *testing.T) {
-	assert := assert.New(t)
-	var rec = new(MutationRecorder)
-
-	doc := html.NewHTMLDocument(nil)
-
-	assert.NoError(NewObserver(rec, rec).Observe(doc, ChildList), "Error when including ChildList")
-	assert.NoError(
-		NewObserver(rec, rec).Observe(doc, Attributes),
-		"Error when including Attributes",
-	)
-	assert.NoError(NewObserver(rec, rec).Observe(doc, CharacterData),
-		"Error when including CharacterData")
-
-	observer := NewObserver(rec, rec)
-	assert.ErrorIs(observer.Observe(doc), gosterror.TypeError{},
-		"Error when calling with no arguments")
-	assert.ErrorIs(
-		NewObserver(rec, rec).Observe(doc,
-			Subtree,
-			AttributeOldValue,
-			CharacterDataOldValue,
-			AttributeFilter([]string{"dummy"})),
-		gosterror.TypeError{},
-		"Error when _all_ non other options than one of the 3 required is passed",
-	)
-}
-
-func Test(t *testing.T) {
-	suite.Run(t, new(MutationObserverTestSuite))
-}
-
-/*
-TODO: Note this about subtree monitoring:
-
-subtree (optional)
-
-Set to true to extend monitoring to the entire subtree of nodes rooted at
-target. All of the other properties are then extended to all of the nodes in the
-subtree instead of applying solely to the target node. The default value is
-false.
-
-**Note that if a descendant of target is removed, changes in that
-descendant subtree will continue to be observed, until the notification about
-the removal itself has been delivered.**
-
-source: https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver/observe
-
-*/
