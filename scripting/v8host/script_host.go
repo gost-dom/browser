4e1b2ccc066b0733e05fe5f69380ff2728ce1916
Peter Strøiman <peter@stroiman.com>
2025-05-11 08:24:53 +0200

    refactor(v8): Split script host into more files

--- scripting/v8host/script_host.go
+++ scripting/v8host/script_host.go
@@ -1,25 +1,18 @@
 package v8host
 
 import (
-	"bytes"
-	"errors"
 	"fmt"
 	"net/http"
-	"runtime/cgo"
-	"strings"
 	"sync"
 
-	"github.com/gost-dom/browser/dom"
 	"github.com/gost-dom/browser/dom/event"
 	"github.com/gost-dom/browser/html"
 	"github.com/gost-dom/browser/internal/clock"
 	"github.com/gost-dom/browser/internal/constants"
 	"github.com/gost-dom/browser/internal/entity"
 	"github.com/gost-dom/browser/internal/log"
-	"github.com/gost-dom/browser/internal/uievents"
 	"github.com/gost-dom/browser/scripting"
 
-	"github.com/gost-dom/v8go"
 	v8 "github.com/gost-dom/v8go"
 )
 
@@ -58,113 +51,6 @@ type V8ScriptHost struct {
 	globals         globals
 	httpClient      *http.Client
 	contexts        map[*v8.Context]*V8ScriptContext
-}
-
-func (h *V8ScriptHost) netContext(v8ctx *v8.Context) (*V8ScriptContext, bool) {
-	ctx, ok := h.contexts[v8ctx]
-	return ctx, ok
-}
-
-func (h *V8ScriptHost) mustGetContext(v8ctx *v8.Context) *V8ScriptContext {
-	if ctx, ok := h.netContext(v8ctx); ok {
-		return ctx
-	}
-	panic("Unknown v8 context")
-}
-
-type V8ScriptContext struct {
-	htmxLoaded bool
-	host       *V8ScriptHost
-	v8ctx      *v8.Context
-	window     html.Window
-	v8nodes    map[entity.ObjectId]*v8.Value
-	eventLoop  *eventLoop
-	disposers  []disposable
-	clock      *clock.Clock
-	disposed   bool
-}
-
-func (c *V8ScriptContext) cacheNode(obj *v8.Object, node entity.ObjectIder) (*v8.Value, error) {
-	val := obj.Value
-	objectId := node.ObjectId()
-	c.v8nodes[objectId] = val
-	handle := cgo.NewHandle(node)
-	c.addDisposer(handleDisposable(handle))
-	internal := v8.NewValueExternalHandle(c.host.iso, handle)
-	obj.SetInternalField(0, internal)
-	return val, nil
-}
-
-func (c *V8ScriptContext) getInstanceForNode(
-	node entity.ObjectIder,
-) (*v8.Value, error) {
-	iso := c.host.iso
-	if node == nil {
-		return v8.Null(iso), nil
-	}
-	switch n := node.(type) {
-	case *event.Event:
-		switch n.Data.(type) {
-		case event.CustomEventInit:
-			return c.getInstanceForNodeByName("CustomEvent", n)
-		case uievents.PointerEventInit:
-			return c.getInstanceForNodeByName("PointerEvent", n)
-		case uievents.MouseEventInit:
-			return c.getInstanceForNodeByName("MouseEvent", n)
-		case uievents.UIEventInit:
-			return c.getInstanceForNodeByName("UIEvent", n)
-		default:
-			return c.getInstanceForNodeByName("Event", n)
-		}
-	case dom.Element:
-		if constructor, ok := scripting.HtmlElements[strings.ToLower(n.TagName())]; ok {
-			return c.getInstanceForNodeByName(constructor, n)
-		}
-		return c.getInstanceForNodeByName("Element", n)
-	case html.HTMLDocument:
-		return c.getInstanceForNodeByName("HTMLDocument", n)
-	case dom.Document:
-		return c.getInstanceForNodeByName("Document", n)
-	case dom.DocumentFragment:
-		return c.getInstanceForNodeByName("DocumentFragment", n)
-	case dom.Node:
-		return c.getInstanceForNodeByName("Node", n)
-	case dom.Attr:
-		return c.getInstanceForNodeByName("Attr", n)
-	default:
-		fmt.Println("ERROR!", n)
-		panic(fmt.Sprintf("Cannot lookup node: %V", n))
-	}
-}
-
-func (c *V8ScriptContext) getInstanceForNodeByName(
-	constructor string,
-	node entity.ObjectIder,
-) (*v8.Value, error) {
-	iso := c.host.iso
-	if node == nil {
-		return v8.Null(iso), nil
-	}
-	prototype, ok := c.host.globals.namedGlobals[constructor]
-	if !ok {
-		panic("Bad constructor name")
-	}
-	value, err := prototype.InstanceTemplate().NewInstance(c.v8ctx)
-	if err == nil {
-		objectId := node.ObjectId()
-		if cached, ok := c.v8nodes[objectId]; ok {
-			return cached, nil
-		}
-		return c.cacheNode(value, node)
-	}
-	return nil, err
-}
-
-func (c *V8ScriptContext) getCachedNode(this *v8.Object) (entity.ObjectIder, bool) {
-
-	h := this.GetInternalField(0).ExternalHandle()
-	r, ok := h.Value().(entity.ObjectIder)
-	return r, ok
 }
 
 type jsConstructorFactory = func(*V8ScriptHost) *v8.FunctionTemplate
@@ -382,138 +268,4 @@ func (host *V8ScriptHost) NewContext(w html.Window) html.ScriptContext {
 	host.addContext(context)
 
 	return context
-}
-
-func (ctx *V8ScriptContext) Clock() html.Clock { return ctx.clock }
-
-func (host *V8ScriptHost) addContext(ctx *V8ScriptContext) {
-	host.mu.Lock()
-	defer host.mu.Unlock()
-	host.contexts[ctx.v8ctx] = ctx
-}
-
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (ctx *V8ScriptContext) Close() {
-	if ctx.disposed {
-		panic("Context already disposed")
-	}
-	ctx.disposed = true
-	ctx.host.inspector.ContextDestroyed(ctx.v8ctx)
-	log.Debug(ctx.host.logger,
-		"ScriptContext: Dispose")
-	for _, dispose := range ctx.disposers {
-		dispose.dispose()
-	}
-	delete(ctx.host.contexts, ctx.v8ctx)
-	ctx.v8ctx.Close()
-}
-
-func (ctx *V8ScriptContext) addDisposer(disposer disposable) {
-	ctx.disposers = append(ctx.disposers, disposer)
-}
-
-func (ctx *V8ScriptContext) runScript(script string) (res *v8.Value, err error) {
-	res, err = ctx.v8ctx.RunScript(script, "")
-	ctx.eventLoop.tick()
-	return
-}
-
-func (ctx *V8ScriptContext) Run(script string) error {
-	return ctx.compile(script).Run()
-}
-
-func (ctx *V8ScriptContext) Eval(script string) (any, error) {
-	return ctx.compile(script).Eval()
-}
-
-func (ctx *V8ScriptContext) compile(script string) html.Script {
-	return V8Script{ctx, script}
-}
-
-func (ctx *V8ScriptContext) Compile(script string) (html.Script, error) {
-	return ctx.compile(script), nil
-}
-
-func (ctx *V8ScriptContext) DownloadScript(url string) (html.Script, error) {
-	resp, err := ctx.host.httpClient.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	buf := bytes.NewBuffer([]byte{})
-	buf.ReadFrom(resp.Body)
-	script := string(buf.Bytes())
-
-	if resp.StatusCode != 200 {
-		err := fmt.Errorf(
-			"v8host: ScriptContext: bad status code: %d, downloading %s",
-			resp.StatusCode,
-			url,
-		)
-		ctx.host.logger.Error("Script download error", "err", err, "body", script)
-		return nil, err
-	}
-	return ctx.Compile(script)
-}
-
-type V8Script struct {
-	ctx    *V8ScriptContext
-	script string
-}
-
-func (s V8Script) Run() error {
-	_, err := s.ctx.runScript(s.script)
-	return err
-}
-
-func (s V8Script) Eval() (any, error) {
-
-	result, err := s.ctx.runScript(s.script)
-	if err == nil {
-		return v8ValueToGoValue(result)
-	}
-	return nil, err
-}
-
-func v8ValueToGoValue(result *v8go.Value) (any, error) {
-	if result == nil {
-		return nil, nil
-	}
-	if result.IsBoolean() {
-		return result.Boolean(), nil
-	}
-	if result.IsInt32() {
-		return result.Int32(), nil
-	}
-	if result.IsString() {
-		return result.String(), nil
-	}
-	if result.IsNull() {
-		return nil, nil
-	}
-	if result.IsUndefined() {
-		return nil, nil
-	}
-	if result.IsArray() {
-		obj, _ := result.AsObject()
-		length, err := obj.Get("length")
-		l := length.Uint32()
-		errs := make([]error, l+1)
-		errs[0] = err
-		result := make([]any, l)
-		for i := uint32(0); i < l; i++ {
-			val, err := obj.GetIdx(i)
-			if err == nil {
-				result[i], err = v8ValueToGoValue(val)
-			}
-			errs[i+1] = err
-		}
-		return result, errors.Join(errs...)
-	}
-	return nil, fmt.Errorf("Value not yet supported: %v", *result)
 }
