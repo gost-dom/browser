939d67acb5431bdd2940a7afafaf9c548e5e1e81
Peter Strøiman <peter@stroiman.com>
2025-05-16 11:09:35 +0200

    WIP Datastar test

---
+++ scripting/v8host/datastar_test.go
@@ -0,0 +1,22 @@
+package v8host_test
+
+import (
+	"log/slog"
+	"testing"
+
+	"github.com/gost-dom/browser"
+	app "github.com/gost-dom/browser/internal/test/integration/test-app"
+	"github.com/gost-dom/browser/internal/testing/gosttest"
+	"github.com/gost-dom/browser/internal/testing/htmltest"
+)
+
+func TestDatastar(t *testing.T) {
+	server := app.CreateServer()
+	b := htmltest.NewBrowserHelper(t, browser.New(
+		browser.WithHandler(server),
+		browser.WithLogger(gosttest.NewTestLogger(t, gosttest.MinLogLevel(slog.LevelDebug))),
+	))
+	win := b.OpenWindow("https://example.com/ds")
+	win.HTMLDocument().GetHTMLElementById("fetch-events-button").Click()
+	t.Log(win.HTMLDocument().Body().OuterHTML())
+}
