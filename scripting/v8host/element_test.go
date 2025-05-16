6d5f64c5a010e5bddb8b9b79587571e45d02f1bc
Peter Strøiman <peter@stroiman.com>
2025-05-16 09:44:41 +0200

    test: Convert v8 element tests to suite

--- scripting/v8host/element_test.go
+++ scripting/v8host/element_test.go
@@ -1,81 +1,78 @@
 package v8host_test
 
 import (
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	// . "github.com/gost-dom/browser/scripting"
+	"testing"
+
+	"github.com/gost-dom/browser/internal/test/scripttests"
+	. "github.com/gost-dom/browser/internal/testing/gomega-matchers"
+	"github.com/gost-dom/browser/internal/testing/gosttest"
+	"github.com/gost-dom/browser/scripting/v8host"
+	"github.com/stretchr/testify/suite"
 )
 
-var _ = Describe("V8 Element", func() {
-	It("It should be a direct descendant of Node", func() {
-		ctx := NewTestContext()
-		Expect(
-			ctx.Eval("Object.getPrototypeOf(Element.prototype) === Node.prototype"),
-		).To(BeTrue())
-	})
+type ElementTestSuite struct {
+	*scripttests.ScriptHostSuite
+}
 
-	It("Should have nodeType == 1", func() {
-		ctx := NewTestContext(LoadHTML(`<div id="1" class="foo"></div>`))
-		Expect(ctx.Eval("document.body.nodeType")).To(BeEquivalentTo(1))
-	})
+func TestElement(t *testing.T) {
+	logger := gosttest.NewTestLogger(t)
+	host := v8host.New(v8host.WithLogger(logger))
+	suite.Run(t,
+		&ElementTestSuite{scripttests.NewScriptHostSuite(host)},
+	)
+}
 
-	It("Supports textContent property", func() {
-		Skip("Add test?")
-	})
+func (s *ElementTestSuite) TestBasicElementProperties() {
+	s.Expect(
+		s.Eval("Object.getPrototypeOf(Element.prototype) === Node.prototype"),
+	).To(BeTrue(), "Element by be a direct descendant of node")
 
-	Describe("Attributes", func() {
-		It("Should support set/getAtribute", func() {
-			ctx := NewTestContext(LoadHTML(`<div id="1" class="foo"></div>`))
-			Expect(ctx.Eval(
-				`document.getElementById("1").getAttribute("class")`,
-			)).To(Equal("foo"))
-			Expect(ctx.Eval(`
-				const e = document.getElementById("1")
-				e.setAttribute("data-foo", "bar");
-				e.getAttribute("data-foo")`,
-			)).To(Equal("bar"))
-		})
+	s.Expect(s.Eval("document.body.nodeType")).To(BeEquivalentTo(1), "Element must have nodetype 1")
+	s.Expect(s.Eval("document.body.nodeType === Node.ELEMENT_NODE")).
+		To(BeTrue(), "Element.nodeType must equal Node.ELEMENT_NODE")
+}
 
-		It("Should return null when getting non-existing attribute", func() {
-			ctx := NewTestContext()
-			Expect(
-				ctx.Eval(`document.createElement("div").getAttribute("dummy") === null`),
-			).To(BeTrue())
+func (s *ElementTestSuite) TestTextContent() {
+	s.MustRunScript("document.body.textContent = 'text content'")
+	s.Assert().Equal(
+		"text content",
+		s.Window.Document().Body().TextContent())
+}
 
-		})
+func (s *ElementTestSuite) TestAttributes() {
+	s.MustLoadHTML(`<div id="1" class="foo"></div>`)
+	s.Expect(s.Eval(
+		`document.getElementById("1").getAttribute("class")`,
+	)).To(Equal("foo"))
+	s.Expect(s.Eval(`
+		const e = document.getElementById("1")
+		e.setAttribute("data-foo", "bar");
+		e.getAttribute("data-foo")`,
+	)).To(Equal("bar"))
 
-		It("Should support hasAtribute", func() {
-			ctx := NewTestContext(LoadHTML(`<div id="1" class="foo"></div>`))
-			Expect(ctx.Eval(
-				`document.getElementById("1").hasAttribute("class")`,
-			)).To(BeTrue())
-			Expect(ctx.Eval(
-				`document.getElementById("1").hasAttribute("foo")`,
-			)).To(BeFalse())
-		})
-	})
+	s.Expect(s.Eval(`e.getAttribute('non-existing') === null`)).
+		To(BeTrue(), "Reading a non-existing attribute should return null")
 
-	It("Should support insertAdjacentHTML", func() {
-		ctx := NewTestContext(LoadHTML(`<div id="1" class="foo"></div>`))
-		Expect(ctx.Eval(
-			`document.getElementById("1").insertAdjacentHTML("beforebegin", "<p>foo</p>")`,
-		)).Error().ToNot(HaveOccurred())
-		Expect(
-			ctx.Window().Document().Body().OuterHTML(),
-		).To(Equal(`<body><p>foo</p><div id="1" class="foo"></div></body>`))
-	})
+	s.Expect(s.Eval(`e.hasAttribute("data-foo")`)).
+		To(BeTrue(), "hasAttribute of existing attribute")
 
-	It("Should have a querySelector function", func() {
-		ctx := NewTestContext(LoadHTML(`<div id="1" class="foo"></div>`))
-		Expect(ctx.Eval(
-			`typeof document.getElementById("1").querySelector`,
-		)).To(Equal("function"))
-	})
+	s.Expect(s.Eval(`e.hasAttribute("non-existing-attribute")`)).
+		To(BeFalse(), "hasAttribute of non-existing attribute")
+}
 
-	It("Should have a querySelectorAll function", func() {
-		ctx := NewTestContext(LoadHTML(`<div id="1" class="foo"></div>`))
-		Expect(ctx.Eval(
-			`typeof document.getElementById("1").querySelectorAll`,
-		)).To(Equal("function"))
-	})
-})
+func (s *ElementTestSuite) TestInsertAdjacentHTML() {
+	s.MustLoadHTML(`<div id="1" class="foo"></div>`)
+	s.Expect(s.Eval(
+		`document.getElementById("1").insertAdjacentHTML("beforebegin", "<p>foo</p>")`,
+	)).Error().ToNot(HaveOccurred())
+	s.Expect(
+		s.Window.Document().Body().OuterHTML(),
+	).To(Equal(`<body><p>foo</p><div id="1" class="foo"></div></body>`))
+}
+
+func (s *ElementTestSuite) TestQuerySelector() {
+	s.MustLoadHTML(`<div id="1" class="foo"></div>`)
+	s.MustRunScript(`const e = document.getElementById("1")`)
+	s.Expect(s.Eval(`typeof e.querySelector`)).To(Equal("function"))
+	s.Expect(s.Eval(`typeof e.querySelectorAll`)).To(Equal("function"))
+}
