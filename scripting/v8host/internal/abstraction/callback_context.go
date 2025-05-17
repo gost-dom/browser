3b7eaf030493c72e5e0bba1449ce8b17259ac02d
Peter Strøiman <peter@stroiman.com>
2025-05-17 11:14:51 +0200

    refactor: Support host-agnostic script implementations
    Work on a framework allowing code to setup the JavaScript global scope
    implemented in native objects in a way that is agnostic of the script
    engine.
    
    This makes the script engine more pluggable, and allows 3rd party
    modules to implement non-essesntial APIs.

---
+++ scripting/v8host/internal/abstraction/callback_context.go
@@ -0,0 +1,73 @@
+package abstraction
+
+import "errors"
+
+var ErrMissingArgument = errors.New("missing argument")
+var ErrNoInternalValue = errors.New("object does not have an internal instance")
+
+// CallbackContext represents the execution context of a JavaScript function
+// or handler callback. For example
+//
+// - Calling a native function
+// - Getting or setting an accessor property backed by a native function
+// - Named or indexed handler callbacks / interceptors.
+type CallbackContext interface {
+	// ConsumeRequiredArg pulls argument from the list of required arguments. If
+	// there are no more arguments to consume, an error wrapping ErrMissingArgument error will be
+	// returned. (e.g., if you call the method 3
+	// times, but only two arguments were passed). The actual error will contain
+	// the name of the argument in the error message.
+	//
+	// Client code does not need to deal with the actual error, and should
+	// normally just return it as is.
+	//  The script engine has the responsibility to translate the error to the
+	//  proper JavaScript error. If client code wishes to create a new error
+	//  type it must wrap the original error if the default JavaScript Error
+	//  should be returned.
+	//
+	// The function is intended for parsing required arguments in the specs.
+	ConsumeRequiredArg(name string) (Value, error)
+
+	// ConsumeOptionalArg pulls an argument from the list or passed arguments.
+	// Return value found indicates whether an argument was available.
+	//
+	// It is not an error to continue calling ConsumeOptionalArg after it
+	// returns false, permitting client code to read all optional arguments
+	// before deciding what to do.
+	ConsumeOptionalArg() (arg Value, found bool)
+
+	// ConsumeRestArgs returns all remaining arguments as a slice of values.
+	//
+	// This is intended for implementing functions with variadic arguments,
+	// e.g., [Element.append]
+	//
+	// [Element.append]: https://developer.mozilla.org/en-US/docs/Web/API/Element/append
+	ConsumeRestArgs() []Value
+
+	// InternalInstance returns the Go value that is wrapped by "this", with
+	// "this" referring the the JavaScript value of "this". If the object does
+	// not contain an internal Go value an [ErrNoInternalValue] error is
+	// returned.
+	InternalInstance() (any, error)
+
+	ReturnWithValue(Value) CallbackRVal
+	ReturnWithError(error) CallbackRVal
+
+	// ValueFactory returns a "factory" that can be used to produce JavaScript
+	// values.
+	ValueFactory() ValueFactory
+}
+
+type Value interface {
+	AsString() string
+}
+
+type CallbackRVal interface{}
+
+type FunctionCallback func(CallbackContext) CallbackRVal
+
+// ValueFactory allows creating JavaScript values from Go values
+type ValueFactory interface {
+	Null() Value
+	String(string) Value
+}
---
+++ scripting/v8host/internal/abstraction/package.go
@@ -0,0 +1,17 @@
+// Package abstraction provides an abstraction on top of V8.
+//
+// The purpose is to allow a modular approach to implementing client-side APIs,
+// where the modules don't need to address a specific script engine; and client
+// code can switch between script engines.
+//
+// The ability to switch script engine seems like a really good idea, as you
+// have two different priorities you might want to switch between:
+//
+// - Using an engine that you know contains latest ES standards (V8)
+// - Using a pure Go engine, simplifying the build pipeline.
+//
+// Additionally, if other projects would support SpiderMonkey (Firefox) and
+// JavaScriptCore (Safari), you could verify your code using the script engines
+// of the different browsers. (Do note, that it is outside the scope of this
+// project to simulate the differences of different browsers).
+package abstraction
