45413f5dca940e496cb843c3b7ae00cd15973b85
Peter Strøiman <peter@stroiman.com>
2025-05-18 15:07:59 +0200

    docs: Fix typo in package documentation

--- scripting/v8host/internal/abstraction/package.go
+++ scripting/v8host/internal/abstraction/package.go
@@ -2,12 +2,12 @@
 //
 // The purpose is to allow a modular approach to implementing client-side APIs,
 // where the modules don't need to address a specific script engine; and client
-// code can witch between script engines.
+// code can switch between script engines.
 //
 // The ability to switch script engine seems like a really good idea, as you
 // have two different priorities you might want to switch between:
 //
-// - Using an engine that you know contains lates ES standards (V8)
+// - Using an engine that you know contains latest ES standards (V8)
 // - Using a pure Go engine, simplifying the build pipeline.
 //
 // Additionally, if other projects would support SpiderMonkey (Firefox) and
