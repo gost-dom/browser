f5460e210c01a01ac864602a7e5a9f4453ebc7cf
Peter Strøiman <peter@stroiman.com>
2025-05-19 12:36:55 +0200

    WIP attempt to log rejected promises

--- internal/test/integration/test-app/content/public/ds/plugins/official/backend/actions/get.js
+++ internal/test/integration/test-app/content/public/ds/plugins/official/backend/actions/get.js
@@ -1,12 +1,15 @@
 // Icon: ic:baseline-get-app
 // Slug: Use a GET request to fetch data from a server using Server-Sent Events matching the Datastar SDK interface
 // Description: Remember, SSE is just a regular SSE request but with the ability to send 0-inf messages to the client.
-import { PluginType, } from '../../../../engine/types';
-import { sse } from './sse';
+import { PluginType } from "../../../../engine/types";
+import { sse } from "./sse";
 export const GET = {
-    type: PluginType.Action,
-    name: 'get',
-    fn: async (ctx, url, args) => {
-        return sse(ctx, 'GET', url, { ...args });
-    },
+  type: PluginType.Action,
+  name: "get",
+  fn: async (ctx, url, args) => {
+    console.log("GET!");
+    const res = sse(ctx, "GET", url, { ...args });
+    console.log("GET DONE ", res, new Error().stack);
+    return res;
+  },
 };
