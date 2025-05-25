2223b9ebc98b947ff76394aa326ebfddb7677986
Peter Strøiman <peter@stroiman.com>
2025-05-25 18:17:08 +0200

    Fix package name in package documentation

--- internal/interfaces/html-interfaces/package.go
+++ internal/interfaces/html-interfaces/package.go
@@ -1,6 +1,6 @@
 //go:generate ../../code-gen/code-gen -g gotypes -p htmlinterfaces
 
-// Package urlinterfaces contains internal code representing the IDL interfaces
+// Package htmlinterfaces contains internal code representing the IDL interfaces
 // in the [HTML DOM API]
 //
 // [HTML DOM API]: https://developer.mozilla.org/en-US/docs/Web/API/HTML_DOM_API
