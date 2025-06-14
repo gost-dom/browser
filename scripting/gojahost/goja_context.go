8ae19b8b5086b76f1e8b2951048af9e001def5df
Peter Strøiman <peter@stroiman.com>
2025-06-14 15:32:39 +0200

    wor(goja): Implement named property handler (dynamic object)

--- scripting/gojahost/goja_context.go
+++ scripting/gojahost/goja_context.go
@@ -168,6 +168,21 @@ type gojaClass struct {
 	prototype      *goja.Object
 	indexedHandler *gojaIndexedHandler
 	instanceAttrs  map[string]attributeHandler
+
+	namedHandlerCallbacks *js.NamedHandlerCallbacks[jsTypeParam]
+}
+
+func (c *gojaClass) assertValid() {
+	if c.indexedHandler != nil || c.namedHandlerCallbacks != nil {
+		if c.indexedHandler != nil && c.namedHandlerCallbacks != nil {
+			panic("Goja mapper doesn't support both a named and indexed handler on the same class")
+		}
+		if len(c.instanceAttrs) > 0 {
+			panic(
+				"Goja mapper doesn't support instance attribute accessors or methods when handlers are defined",
+			)
+		}
+	}
 }
 
 func (c *gojaClass) CreateIndexedHandler(opts ...js.IndexedHandlerOption[jsTypeParam]) {
@@ -176,9 +191,17 @@ func (c *gojaClass) CreateIndexedHandler(opts ...js.IndexedHandlerOption[jsTypeP
 		o(&oo)
 	}
 	c.indexedHandler = &gojaIndexedHandler{oo.Getter}
+	c.assertValid()
 }
 
-func (c *gojaClass) CreateNamedHandler(cb ...js.NamedHandlerOption[jsTypeParam]) {}
+func (c *gojaClass) CreateNamedHandler(opts ...js.NamedHandlerOption[jsTypeParam]) {
+	var cbs js.NamedHandlerCallbacks[jsTypeParam]
+	for _, o := range opts {
+		o(&cbs)
+	}
+	c.namedHandlerCallbacks = &cbs
+	c.assertValid()
+}
 
 func (c *gojaClass) CreateInstanceAttribute(
 	name string,
@@ -186,6 +209,7 @@ func (c *gojaClass) CreateInstanceAttribute(
 	setter js.FunctionCallback[jsTypeParam],
 ) {
 	c.instanceAttrs[name] = attributeHandler{c.ctx, name, getter, setter}
+	c.assertValid()
 }
 
 func (c gojaClass) CreatePrototypeMethod(
@@ -225,6 +249,16 @@ func (c gojaClass) CreateIteratorMethod(cb js.FunctionCallback[jsTypeParam]) {
 }
 
 func (c *gojaClass) NewInstance(native any) (js.Object[jsTypeParam], error) {
+	if c.namedHandlerCallbacks != nil {
+		dynObj := gojaDynamicObject{
+			ctx: c.ctx,
+			cbs: *c.namedHandlerCallbacks,
+		}
+		obj := c.ctx.vm.NewDynamicObject(dynObj)
+		dynObj.this = obj
+		obj.SetPrototype(c.prototype)
+		return newGojaObject(c.ctx, obj), nil
+	}
 	obj := c.ctx.vm.CreateObject(c.prototype)
 	c.installInstance(obj)
 	c.ctx.storeInternal(native, obj)
@@ -239,4 +273,82 @@ func newGojaCallbackContext(
 		gojaCallbackScope{ctx,
 			call.This,
 		}, call.Arguments, 0}
+}
+
+type gojaDynamicObject struct {
+	ctx   *GojaContext
+	this  *goja.Object
+	scope gojaCallbackScope
+	cbs   js.NamedHandlerCallbacks[jsTypeParam]
+}
+
+func (o gojaDynamicObject) Get(key string) goja.Value {
+	if o.cbs.Getter == nil {
+		return nil
+	}
+	res, err := o.cbs.Getter(o.scope, o.scope.ValueFactory().NewString(key))
+	if err == js.NotIntercepted {
+		return nil
+	}
+	if err != nil {
+		panic(err)
+	}
+	return res.Self().value
+}
+
+func (o gojaDynamicObject) Delete(key string) (res bool) {
+	var err error
+	if o.cbs.Deleter != nil {
+		res, err = o.cbs.Deleter(o.scope, o.scope.ValueFactory().NewString(key))
+		if err == js.NotIntercepted {
+			return false
+		}
+		if err != nil {
+			panic(err)
+		}
+	}
+	return res
+}
+
+func (o gojaDynamicObject) Has(key string) (res bool) {
+	if o.cbs.Getter == nil {
+		panic("Must have a getter")
+	}
+	_, err := o.cbs.Getter(o.scope, o.scope.ValueFactory().NewString(key))
+	if err == js.NotIntercepted {
+		return false
+	}
+	if err != nil {
+		panic(err)
+	}
+	return true
+}
+
+func (o gojaDynamicObject) Keys() []string {
+	keys, err := o.cbs.Enumerator(o.scope)
+	if err == js.NotIntercepted {
+		return nil
+	}
+	if err != nil {
+		panic(err)
+	}
+	res := make([]string, len(keys))
+	for i, k := range keys {
+		res[i] = k.String()
+	}
+	return res
+}
+
+func (o gojaDynamicObject) Set(key string, val goja.Value) bool {
+	if o.cbs.Setter == nil {
+		return false
+	}
+	err := o.cbs.Setter(o.scope, o.scope.ValueFactory().NewString(key), newGojaValue(o.ctx, val))
+	if err == js.NotIntercepted {
+		return false
+	}
+	if err != nil {
+		panic(err)
+	}
+	return true
 }
