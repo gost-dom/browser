4243f4887f2553686b01e291441894efbc27b66f
Peter Strøiman <peter@stroiman.com>
2025-06-15 14:29:46 +0200

    work(scripting): Skeleton fetch function

--- scripting/internal/configurator.go
+++ scripting/internal/configurator.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"github.com/gost-dom/browser/scripting/internal/codec"
 	"github.com/gost-dom/browser/scripting/internal/dom"
+	"github.com/gost-dom/browser/scripting/internal/fetch"
 	"github.com/gost-dom/browser/scripting/internal/html"
 	"github.com/gost-dom/browser/scripting/internal/js"
 	"github.com/gost-dom/browser/scripting/internal/uievents"
@@ -12,6 +13,7 @@ import (
 
 func Configure[T any](host js.ScriptEngine[T]) {
 	dom.Configure(host)
+	fetch.Configure(host)
 }
 
 func Bootstrap[T any](reg js.ClassBuilder[T]) {
---
+++ scripting/internal/fetch/fetch.go
@@ -0,0 +1,7 @@
+package fetch
+
+import "github.com/gost-dom/browser/scripting/internal/js"
+
+func Fetch[T any](info js.CallbackContext[T]) (js.Value[T], error) {
+	return nil, nil
+}
