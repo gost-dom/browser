a13c7c045d9815020cf0cdcdcbcc477450863678
Peter Strøiman <peter@stroiman.com>
2025-06-24 00:45:13 +0200

    test: Convert ginkgo to named go test

--- scripting/v8host/dom_parser_test.go
+++ scripting/v8host/dom_parser_test.go
@@ -1,37 +1,27 @@
 package v8host_test
 
 import (
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
+	"testing"
+
+	"github.com/gost-dom/browser/internal/testing/browsertest"
+	. "github.com/gost-dom/browser/internal/testing/gomega-matchers"
+	"github.com/onsi/gomega"
 )
 
-var _ = Describe("DOM Parser", func() {
-	It("Should return a Document", func() {
-		ctx := NewTestContext()
-		Expect(ctx.Eval(`
-			const parser = new DOMParser()
-			const doc = parser.parseFromString("<div id='target'></div>", "text/html")
-		`)).Error().ToNot(HaveOccurred())
-		Expect(
-			ctx.Eval("Object.getPrototypeOf(doc) === HTMLDocument.prototype"),
-		).To(BeTrue(), "result is a Document")
-		Expect(
-			ctx.Eval("doc === window.document"),
-		).To(BeFalse(), "Window.document isn't replaced")
-		Expect(
-			ctx.Eval("doc.getElementById('target') instanceof HTMLDivElement"),
-		).To(BeTrue(), "Element is a div")
-	})
-
-	It("Should return an HTMLDocument", func() {
-		ctx := NewTestContext()
-		Expect(ctx.Eval(`
-			const parser = new DOMParser()
-			const doc = parser.parseFromString("<div id='target'></div>", "text/html")
-		`)).Error().ToNot(HaveOccurred())
-		Skip("HTMLDocument not properly implemented")
-		Expect(
-			ctx.Eval("Object.getPrototypeOf(doc) === HTMLDocument.prototype"),
-		).To(BeTrue(), "result is an HTMLDocument")
-	})
-})
+func TestDomParser(t *testing.T) {
+	win := browsertest.InitWindow(t)
+	g := gomega.NewWithT(t)
+	g.Expect(win.Eval(`
+		const parser = new DOMParser()
+		const doc = parser.parseFromString("<div id='target'></div>", "text/html")
+	`)).Error().ToNot(HaveOccurred())
+	g.Expect(
+		win.Eval("Object.getPrototypeOf(doc) === HTMLDocument.prototype"),
+	).To(BeTrue(), "result is a Document")
+	g.Expect(
+		win.Eval("doc === window.document"),
+	).To(BeFalse(), "Window.document isn't replaced")
+	g.Expect(
+		win.Eval("doc.getElementById('target') instanceof HTMLDivElement"),
+	).To(BeTrue(), "Element is a div")
+}
