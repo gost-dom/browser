82171028fd973e1e0381e9d1731e32e361eac0d3
Peter Strøiman <peter@stroiman.com>
2025-06-27 13:02:49 +0200

    doc: Fix typos in promise package documentation

--- internal/promise/package.go
+++ internal/promise/package.go
@@ -1,14 +1,14 @@
-// Package promise provides a Go way to represent the concept of a Promise
+// Package promise provides a Go way to represent the concept of a Promise.
 //
-// In JavaScript, a promise represents an operation that will fullfill with a
+// In JavaScript, a promise represents an operation that will fulfill with a
 // value in the future, or produce an error. The outcome of the operation is
 // represented by generic type [Result], containing either an error or a
 // fulfilled value. The generic [Promise] type is just a channel where the value
 // will be received.
 //
-// This package not intended to provide be a full implementation of JavaScript
-// promises. The intention is to simplify implementation of web APIs that
-// returns promises, e.g. the [fetch API].
+// This package is not intended to provide be a full implementation of
+// JavaScript promises. The intention is to simplify implementation of web APIs
+// that returns promises, e.g. the [fetch API].
 //
 // [fetch API]: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
 package promise
