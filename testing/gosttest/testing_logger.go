1fb6421b5037eff8b35d87537301d7465903121a
Peter Strøiman <peter@stroiman.com>
2025-07-18 10:11:32 +0200

    feat: Improve TestLogger, how it renders attribute groups

--- testing/gosttest/testing_logger.go
+++ testing/gosttest/testing_logger.go
@@ -46,10 +46,11 @@ func (l testingLogHandler) Handle(ctx context.Context, r slog.Record) error {
 	}
 	l.TB.Helper()
 	var b strings.Builder
+	var w attrWriter
 	r.Attrs(func(a slog.Attr) bool {
-		b.WriteString("\t")
-		b.WriteString(a.String())
-		b.WriteString("\n")
+		if !a.Equal(slog.Attr{}) {
+			w.write(&b, a)
+		}
 		return true
 	})
 	if r.Level < slog.LevelError || l.allowErrors {
@@ -104,4 +105,71 @@ func NewTestingLogger(t testing.TB, opts ...TestingLogHandlerOption) *slog.Logge
 		handler.close()
 	})
 	return slog.New(handler)
+}
+
+// attrWrite writes a single [slog.Attr] to a [strings.Builder] in the form
+// "key=value". Groups are rendered in a hirarchical structure
+type attrWriter struct {
+	groups []string
+}
+
+func (w attrWriter) write(b *strings.Builder, a slog.Attr) {
+	if a.Value.Kind() == slog.KindGroup {
+		as := a.Value.Group()
+		if len(as) == 0 {
+			return
+		}
+		w.writeGroup(b, a)
+		return
+	}
+	w.writePrefix(b)
+	b.WriteString(a.Key)
+	b.WriteString(": ")
+	b.WriteString(a.Value.String())
+	b.WriteString("\n")
+}
+
+func (w attrWriter) writePrefix(b *strings.Builder) {
+	b.WriteString("\t")
+
+	// An alternate implementation, writes groups as a dotted list, e.g.
+	// instead of:
+	//
+	// 	res
+	// 		- status=200
+	// 		- header
+	// 			- Set-Cookie=...
+	//
+	// It will write
+	//
+	// 	res.status=200
+	// 	res.header.Set-Cookie=...
+
+	// for _, g := range w.groups {
+	// 	b.WriteString(g)
+	// 	b.WriteByte('.')
+	// }
+
+	if len(w.groups) == 0 {
+		return
+	}
+	for range w.groups {
+		b.WriteString("\t")
+	}
+	b.WriteString("- ")
+}
+
+func (w attrWriter) writeGroup(b *strings.Builder, a slog.Attr) {
+	// Write group header - remove if using the alternate version
+	w.writePrefix(b)
+	b.WriteString(a.Key)
+	b.WriteString("\n")
+
+	g := w.groups
+	w.groups = append(w.groups, a.Key)
+	defer func() { w.groups = g }()
+
+	for _, a := range a.Value.Group() {
+		w.write(b, a)
+	}
 }
