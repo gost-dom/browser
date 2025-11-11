2596d487a9da55b685d076d7cfceefdb97839680
Peter Strøiman <peter@stroiman.com>
2025-11-11 16:02:04 +0100

    test: create test verifying null signal/body doesn't throw
    PR Review indicated that decoding `null` values for these two options
    could result in an error. The test was meant to provoke the error and
    drive a fix, but apparently the condition will be filtered elsewhere.
    
    Code should be improved, so it is more obvious that this will not result
    in an error when looking at the implementation directly; but for now,
    the test is allowed to stay as a safe guard.

--- scripting/internal/fetch/fetch_test.go
+++ scripting/internal/fetch/fetch_test.go
@@ -8,13 +8,26 @@ import (
 	"github.com/gost-dom/browser/internal/testing/gosttest"
 	"github.com/gost-dom/browser/internal/testing/htmltest"
 	"github.com/onsi/gomega"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFetch(t *testing.T) {
 	g := gomega.NewWithT(t)
-	b := browsertest.InitBrowser(t, gosttest.HttpHandlerMap{
+	rec := gosttest.NewHTTPRequestRecorder(t, gosttest.HttpHandlerMap{
 		"/file.json": gosttest.StaticJSON(`{"foo": "Foo value", "bar": "Bar value"}`),
 	})
+	b := browsertest.InitBrowser(t, rec)
 	w := htmltest.NewWindowHelper(t, b.NewWindow())
 	g.Expect(w.Eval("typeof fetch")).To(Equal("function"))
+
+	w.MustRun(`
+		let result;
+		fetch("http://example.com/file.json", { body: null }).
+			then(response => response.json()).
+			then(js => { result = js });
+	`)
+	err := w.Clock().ProcessEvents(t.Context())
+	assert.NoError(t, err)
+	assert.Equal(t, "Foo value", w.MustEval("result.foo"))
+	assert.Equal(t, "Bar value", w.MustEval("result.bar"))
 }
