2a4e2a8561be843b59e08b6421e6e81d940dcc87
Peter Strøiman <peter@stroiman.com>
2025-11-16 18:17:04 +0100

    Add skeleton web platform test

---
+++ internal/test/wpt/go.mod
@@ -0,0 +1,20 @@
+module github.com/gost-dom/browser/internal/test/wpt
+
+go 1.25.3
+
+require (
+	github.com/Marlliton/slogpretty v0.1.3
+	github.com/gost-dom/browser v0.9.2
+)
+
+require (
+	github.com/gost-dom/css v0.1.0 // indirect
+	github.com/gost-dom/v8go v0.0.0-20250712111039-fd213ddc42d7 // indirect
+	github.com/tommie/v8go/deps/android_amd64 v0.0.0-20251007175045-97bcf8e7d6ed // indirect
+	github.com/tommie/v8go/deps/android_arm64 v0.0.0-20251007175045-97bcf8e7d6ed // indirect
+	github.com/tommie/v8go/deps/darwin_amd64 v0.0.0-20251007175045-97bcf8e7d6ed // indirect
+	github.com/tommie/v8go/deps/darwin_arm64 v0.0.0-20251007175045-97bcf8e7d6ed // indirect
+	github.com/tommie/v8go/deps/linux_amd64 v0.0.0-20251007175045-97bcf8e7d6ed // indirect
+	github.com/tommie/v8go/deps/linux_arm64 v0.0.0-20251007175045-97bcf8e7d6ed // indirect
+	golang.org/x/net v0.47.0 // indirect
+)
--- internal/test/wpt/main.go
+++ internal/test/wpt/main.go
@@ -5,15 +5,23 @@ import (
 	"log/slog"
 	"os"
 
+	"github.com/Marlliton/slogpretty"
 	"github.com/gost-dom/browser"
+	"github.com/gost-dom/browser/dom"
 )
 
 func main() {
-	h := slog.NewTextHandler(os.Stdout, nil)
+	opts := slogpretty.DefaultOptions()
+	opts.Level = slog.LevelDebug
+	opts.Multiline = true
+	h := slogpretty.New(os.Stdout, opts)
 	log := slog.New(h)
+
+	log.Info("Start test run")
 	b := browser.New(browser.WithLogger(log))
 	ctx := context.Background()
-	win, err := b.Open("https://wpt.live/dom/nodes/CharacterData-appendChild.html")
+	win, err := b.Open("https://wpt.live/dom/nodes/Document-getElementById.html")
+
 	if err != nil {
 		log.Error("Error opening window", "err", err)
 		os.Exit(1)
@@ -22,5 +30,16 @@ func main() {
 	if err != nil {
 		log.Error("Error processing events", "err", err)
 	}
-	log.Info("Document", "html", win.Document().DocumentElement().OuterHTML())
+
+	rows, _ := win.Document().QuerySelectorAll("#results > tbody > tr")
+	for _, row := range rows.All() {
+		tds, _ := row.(dom.Element).QuerySelectorAll("td")
+		result := tds.Item(0).TextContent()
+		name := tds.Item(1).TextContent()
+		if result == "Pass" {
+			log.Info("PASS", "status", result, "test", name)
+		} else {
+			log.Error("FAIL", "status", result, "test", name)
+		}
+	}
 }
