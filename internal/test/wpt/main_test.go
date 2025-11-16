280a3e6174c3bd6c17e1786f68aa0392596225c5
Peter Strøiman <peter@stroiman.com>
2025-11-16 18:48:19 +0100

    test: Create test modules for web platform tests

---
+++ internal/test/wpt/main_test.go
@@ -0,0 +1,32 @@
+package main_test
+
+import (
+	"testing"
+
+	"github.com/gost-dom/browser"
+	"github.com/gost-dom/browser/dom"
+	"github.com/gost-dom/browser/testing/gosttest"
+)
+
+func TestWebApplicationTests(t *testing.T) {
+	b := browser.New(browser.WithLogger(gosttest.NewTestingLogger(t)))
+	t.Cleanup(b.Close)
+	win, err := b.Open("https://wpt.live/dom/nodes/Document-getElementById.html")
+	if err != nil {
+		t.Fatalf("Error opening window: %v", err)
+	}
+	if err = win.Clock().ProcessEvents(t.Context()); err != nil {
+		t.Fatalf("Error processing events: %v", err)
+	}
+
+	rows, _ := win.Document().QuerySelectorAll("#results > tbody > tr")
+	for _, row := range rows.All() {
+		tds, _ := row.(dom.Element).QuerySelectorAll("td")
+		result := tds.Item(0).TextContent()
+		name := tds.Item(1).TextContent()
+		if result == "Fail" {
+			errMsg := tds.Item(2).TextContent()
+			t.Errorf("Fail: %s\n%s", name, errMsg)
+		}
+	}
+}
