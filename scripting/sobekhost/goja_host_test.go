76557b92e9f39a21c132bb829a3537b2498b7be6
Peter Strøiman <peter@stroiman.com>
2025-11-18 22:25:02 +0100

    work: Create prototype ESM implementation for sobek

--- scripting/sobekhost/goja_host_test.go
+++ scripting/sobekhost/goja_host_test.go
@@ -3,20 +3,15 @@ package sobekhost_test
 import (
 	"testing"
 
-	"github.com/gost-dom/browser/html"
 	"github.com/gost-dom/browser/internal/test/scripttests"
 	"github.com/gost-dom/browser/scripting/sobekhost"
 )
 
-type scriptHostFactory struct{}
-
-func (f scriptHostFactory) NewHost(
-	html.ScriptEngineOptions,
-) html.ScriptHost {
-	return sobekhost.New()
-}
-
 func TestSobekHost(t *testing.T) {
 	t.Parallel()
-	scripttests.RunSuites(t, scriptHostFactory{})
+	scripttests.RunSuites(t, sobekhost.GojaEngine{})
+}
+
+func TestESMSupport(t *testing.T) {
+	scripttests.RunModuleSuite(t, sobekhost.GojaEngine{})
 }
