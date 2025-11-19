7701d2d32e4384eaf95b3cf901ac216956a3699f
Peter Strøiman <peter@stroiman.com>
2025-11-19 21:06:01 +0100

    refactor(sobek): Rename gojaPromise -> promise

---
+++ scripting/sobekhost/promise.go
@@ -0,0 +1,22 @@
+package sobekhost
+
+import (
+	"github.com/gost-dom/browser/scripting/internal/js"
+)
+
+type promise struct {
+	js.Value[jsTypeParam]
+	resolve func(any) error
+	reject  func(any) error
+}
+
+func newPromise(ctx *scriptContext) promise {
+	p, resolve, reject := ctx.vm.NewPromise()
+	return promise{
+		newGojaValue(ctx, ctx.vm.ToValue(p)),
+		resolve, reject,
+	}
+}
+
+func (p promise) Resolve(value js.Value[jsTypeParam]) { p.resolve(value.Self().value) }
+func (p promise) Reject(value js.Value[jsTypeParam])  { p.reject(value.Self().value) }
