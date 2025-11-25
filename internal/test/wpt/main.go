069e89bd3b9222e583f3795f7742ddf4cbd96662
Peter Strøiman <peter@stroiman.com>
2025-11-26 00:23:20 +0100

    test: Extract WPT page behaviour

--- internal/test/wpt/main.go
+++ internal/test/wpt/main.go
@@ -2,44 +2,78 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"os"
+	"time"
 
 	"github.com/Marlliton/slogpretty"
 	"github.com/gost-dom/browser"
 	"github.com/gost-dom/browser/dom"
+	"github.com/gost-dom/browser/html"
+	"github.com/gost-dom/browser/v8browser"
 )
+
+type WebPlatformTest string
+
+func (s WebPlatformTest) Run(
+	ctx context.Context,
+	l *slog.Logger,
+) (res []WebPlatformTestCase, err error) {
+	b := v8browser.New(
+		browser.WithContext(ctx),
+		browser.WithLogger(l),
+	)
+	defer b.Close()
+	var win html.Window
+
+	win, err = b.Open("https://wpt.live/dom/nodes/Document-getElementById.html")
+	if err == nil {
+		err = win.Clock().ProcessEvents(ctx)
+	}
+	if err != nil {
+		return
+	}
+	r, _ := win.Document().QuerySelectorAll("#results > tbody > tr")
+	rows := r.All()
+	res = make([]WebPlatformTestCase, len(rows))
+	for i, row := range rows {
+		tds, _ := row.(dom.Element).QuerySelectorAll("td")
+		res[i] = WebPlatformTestCase{
+			Success: tds.Item(0).TextContent() == "Pass",
+			Name:    tds.Item(1).TextContent(),
+			Msg:     tds.Item(2).(dom.Element).OuterHTML(),
+		}
+	}
+	return
+}
+
+type WebPlatformTestCase struct {
+	Name    string
+	Success bool
+	Msg     string
+}
 
 func main() {
 	opts := slogpretty.DefaultOptions()
-	opts.Level = slog.LevelDebug
 	opts.Multiline = true
 	h := slogpretty.New(os.Stdout, opts)
 	log := slog.New(h)
 
-	log.Info("Start test run")
-	b := browser.New(browser.WithLogger(log))
-	ctx := context.Background()
-	win, err := b.Open("https://wpt.live/dom/nodes/Document-getElementById.html")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
 
+	suite := WebPlatformTest("https://wpt.live/dom/nodes/Document-getElementById.html")
+	res, err := suite.Run(ctx, log)
 	if err != nil {
-		log.Error("Error opening window", "err", err)
+		fmt.Fprintf(os.Stderr, "Test error: %v\n", err)
 		os.Exit(1)
 	}
-	err = win.Clock().ProcessEvents(ctx)
-	if err != nil {
-		log.Error("Error processing events", "err", err)
-	}
-
-	rows, _ := win.Document().QuerySelectorAll("#results > tbody > tr")
-	for _, row := range rows.All() {
-		tds, _ := row.(dom.Element).QuerySelectorAll("td")
-		result := tds.Item(0).TextContent()
-		name := tds.Item(1).TextContent()
-		if result == "Pass" {
-			log.Info("PASS", "status", result, "test", name)
+	for _, row := range res {
+		if row.Success {
+			log.Info("PASS", "test", row.Name)
 		} else {
-			log.Error("FAIL", "status", result, "test", name)
+			log.Error("FAIL", "test", row.Name)
 		}
 	}
 }
