05d1b2773c6e23c571ab85e423f7245e43572082
Peter Strøiman <peter@stroiman.com>
2025-11-25 08:22:21 +0100

    refactor: Fix potential future bug
    Remove field `vm` from sobek iterator. This was never assigned, but
    still used to create sobek values. This only passed because it was
    called with boolean values, which doesn't require the VM.

--- scripting/sobekhost/iterator.go
+++ scripting/sobekhost/iterator.go
@@ -2,11 +2,9 @@ package sobekhost
 
 import (
 	"github.com/gost-dom/browser/scripting/internal/js"
-	"github.com/grafana/sobek"
 )
 
 type iterator struct {
-	vm   *sobek.Runtime
 	next func() (js.Value[jsTypeParam], error, bool)
 	stop func()
 }
