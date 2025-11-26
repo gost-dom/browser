24edbffb0afbaef3501a049573f9de3404f58012
Peter Strøiman <peter@stroiman.com>
2025-11-26 23:10:22 +0100

    test: Move element tests

--- internal/test/scripttests/element_suite.go
+++ internal/test/scripttests/element_suite.go
@@ -3,6 +3,7 @@ package scripttests
 import (
 	"errors"
 
+	"github.com/gost-dom/browser/dom"
 	"github.com/gost-dom/browser/html"
 	. "github.com/gost-dom/browser/internal/testing/gomega-matchers"
 	. "github.com/gost-dom/browser/testing/gomega-matchers"
@@ -15,6 +16,16 @@ type ElementSuite struct {
 
 func NewElementSuite(h html.ScriptEngine) *ElementSuite {
 	return &ElementSuite{ScriptHostSuite: *NewScriptHostSuite(h)}
+}
+
+func (s *ElementSuite) TestBasicElementProperties() {
+	s.Expect(
+		s.Eval("Object.getPrototypeOf(Element.prototype) === Node.prototype"),
+	).To(BeTrue(), "Element by be a direct descendant of node")
+
+	s.Expect(s.Eval("document.body.nodeType")).To(BeEquivalentTo(1), "Element must have nodetype 1")
+	s.Expect(s.Eval("document.body.nodeType === Node.ELEMENT_NODE")).
+		To(BeTrue(), "Element.nodeType must equal Node.ELEMENT_NODE")
 }
 
 func (s *ElementSuite) TestAppendMultipleElements() {
@@ -61,6 +72,7 @@ func (s *ElementSuite) TestSetOuterHTML() {
 
 func (s *ElementSuite) TestAttributes() {
 	s.MustLoadHTML(`<body><div foo="foo-value" bar="bar-value"></div><body>`)
+	s.Assert().Equal("foo-value", s.MustEval(`document.body.firstChild.getAttribute("foo")`))
 	s.MustEval(`
 		const div = document.querySelector("div")
 		const attrs = Array.from(div.attributes)
@@ -69,6 +81,14 @@ func (s *ElementSuite) TestAttributes() {
 	`)
 	s.Assert().Equal("foo,bar", s.MustEval("names"))
 	s.Assert().Equal("foo-value,bar-value", s.MustEval("values"))
+
+	s.Expect(s.Eval(`document.body.getAttribute('non-existing') === null`)).
+		To(BeTrue(), "Reading a non-existing attribute should return null")
+
+	s.Expect(s.Eval(`div.hasAttribute("foo")`)).
+		To(BeTrue(), "hasAttribute of existing attribute")
+	s.Expect(s.Eval(`div.hasAttribute("non-existing-attribute")`)).
+		To(BeFalse(), "hasAttribute of non-existing attribute")
 }
 
 func (s *ElementSuite) TestIDLInterfaceNamesForElements() {
@@ -109,6 +129,47 @@ func (s *ElementSuite) TestChildren() {
 	s.Expect(s.MustEval("length")).To(BeEquivalentTo(2))
 
 	assert.Equal("child-1,child-2", s.MustEval("contents"))
+}
+
+func (s *ElementSuite) TestInsertAdjacentHTML() {
+	s.MustLoadHTML(`<div id="1" class="foo"></div>`)
+	s.Expect(s.Eval(
+		`document.getElementById("1").insertAdjacentHTML("beforebegin", "<p>foo</p>")`,
+	)).Error().ToNot(HaveOccurred())
+	s.Expect(
+		s.Window.Document().Body().OuterHTML(),
+	).To(Equal(`<body><p>foo</p><div id="1" class="foo"></div></body>`))
+}
+
+func (s *ElementSuite) TestQuerySelector() {
+	s.MustLoadHTML(`<div id="1" class="foo"></div>`)
+	s.MustRunScript(`const e = document.getElementById("1")`)
+	s.Expect(s.Eval(`typeof e.querySelector`)).To(Equal("function"))
+	s.Expect(s.Eval(`typeof e.querySelectorAll`)).To(Equal("function"))
+}
+
+func (s *ElementSuite) TestElementSiblings() {
+	win := s.Window
+	s.MustLoadHTML(`
+  	<body>
+		<div id="e-1"></div>
+		text node
+		<div id="e-2"></div>
+		text node
+		<div id="e-3"></div>
+		<script>const e = document.getElementById("e-2")</script>
+	</body>`)
+	s.Expect(win.Eval(`e.previousSibling.nodeType`)).
+		To(BeEquivalentTo(dom.NodeTypeText), "nextSibling node type")
+	s.Expect(win.Eval(`e.previousElementSibling.nodeType`)).
+		To(BeEquivalentTo(dom.NodeTypeElement), "nextElementSibling node type")
+	s.Expect(win.Eval(`e.previousElementSibling.id`)).To(Equal("e-1"), "nextElementSibling id")
+
+	s.Expect(win.Eval(`e.nextSibling.nodeType`)).
+		To(BeEquivalentTo(dom.NodeTypeText), "nextSibling node type")
+	s.Expect(win.Eval(`e.nextElementSibling.nodeType`)).
+		To(BeEquivalentTo(dom.NodeTypeElement), "nextElementSibling node type")
+	s.Expect(win.Eval(`e.nextElementSibling.id`)).To(Equal("e-3"), "nextElementSibling id")
 }
 
 type BeJSInstanceOfMatcher struct {
