901ae4c474532ede5df02c75b7f1a05603fa38fd
Peter Strøiman <peter@stroiman.com>
2025-11-26 23:14:35 +0100

    test: Move event target tests

--- internal/test/scripttests/event_target_test_suite.go
+++ internal/test/scripttests/event_target_test_suite.go
@@ -3,6 +3,7 @@ package scripttests
 import (
 	"github.com/gost-dom/browser/dom/event"
 	"github.com/gost-dom/browser/html"
+	"github.com/onsi/gomega"
 	. "github.com/onsi/gomega"
 )
 
@@ -88,4 +89,49 @@ func (s *EventTargetTestSuite) TestPropagateGoEventToJS() {
 	s.Assert().EqualValues(1, s.MustEval("callCount"))
 	s.Assert().Equal(true, s.MustEval(`Object.getPrototypeOf(event) === CustomEvent.prototype`))
 	s.Assert().Equal("custom", s.MustEval(`event.type`), "type of actual event")
+}
+
+func (s *EventTargetTestSuite) TestV8EventTargetAddRemoveListeners() {
+	win := s.Window
+	s.Expect(win.Eval(`
+		let events = [];
+		let noOfEvents = []
+		const handler = e => { events.push(e) }
+		window.addEventListener("gost", handler)
+		window.dispatchEvent(new CustomEvent("gost"))
+		noOfEvents.push(events.length)
+		window.removeEventListener("gost", handler)
+		window.dispatchEvent(new CustomEvent("gost"))
+		noOfEvents.push(events.length)
+		window.addEventListener("gost", handler)
+		window.dispatchEvent(new CustomEvent("gost"))
+		noOfEvents.push(events.length)
+		noOfEvents
+	`)).To(HaveExactElements([]any{
+		gomega.BeEquivalentTo(1),
+		gomega.BeEquivalentTo(1),
+		gomega.BeEquivalentTo(2),
+	}))
+}
+
+func (s *EventTargetTestSuite) TestV8EventCapture() {
+	win := s.Window
+	s.Expect(win.Eval(`
+		let events = [];
+		let noOfEvents = []
+		const div = document.createElement("div")
+		document.body.appendChild(div)
+		window.addEventListener("gost", e => { events.push("Window bubble. Phase: " + e.eventPhase)})
+		window.addEventListener("gost", e => { events.push("Window capture. Phase: " + e.eventPhase)}, true)
+		div.addEventListener("gost", e => { events.push("Div bubble. Phase: " + e.eventPhase)})
+		div.addEventListener("gost", e => { events.push("Div capture. Phase: " + e.eventPhase)}, {capture:true})
+		div.dispatchEvent(new CustomEvent("gost", { bubbles: true }))
+
+		events
+	`)).To(gomega.HaveExactElements(
+		"Window capture. Phase: 1",
+		"Div capture. Phase: 2",
+		"Div bubble. Phase: 2",
+		"Window bubble. Phase: 3",
+	))
 }
