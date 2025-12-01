4d5ce2f88f4fc7de8662903860273cb8b1a94a1e
Peter Strøiman <peter@stroiman.com>
2025-12-01 22:43:59 +0100

    refactor: Create general JS iterator in Go
    Exposing a JavaScript iterable as a Go iter.Seq makes inspecting JS
    values significantly simpler

--- scripting/internal/fetch/headers.go
+++ scripting/internal/fetch/headers.go
@@ -3,6 +3,7 @@ package fetch
 import (
 	"errors"
 	"fmt"
+	"iter"
 
 	"github.com/gost-dom/browser/internal/fetch"
 	"github.com/gost-dom/browser/scripting/internal/codec"
@@ -26,17 +27,11 @@ func (w Headers[T]) decodeHeadersInit(
 	if v == nil {
 		return nil, nil
 	}
+	res, err = w.parseHeaderIterator2(scope, v)
+	if err == nil || (!errors.Is(err, ErrNotIterable)) {
+		return
+	}
 	if obj, ok := v.AsObject(); ok {
-		var it js.Value[T]
-		it, err = obj.Iterator()
-		if err != nil {
-			return nil, err
-		}
-		if it != nil && !it.IsUndefined() {
-			scope.Logger().
-				Info("Iterator", "str", it.String(), "it", it, "obj", obj, "objStr", obj.String())
-			return w.parseHeaderIterator(scope, obj, it)
-		}
 		var keys []string
 		if keys, err = obj.Keys(); err == nil {
 			res = make([][2]string, len(keys))
@@ -53,64 +48,23 @@ func (w Headers[T]) decodeHeadersInit(
 	return nil, nil
 }
 
-func (w Headers[T]) parseHeaderIterator(
-	scope js.Scope[T],
-	arg js.Object[T],
-	it js.Value[T],
-) ([][2]string, error) {
-	itFn, ok := it.AsFunction()
-	if !ok {
-		return nil, errors.New("Not callable")
+func (w Headers[T]) parseHeaderIterator2(
+	scope js.Scope[T], val js.Value[T],
+) (res [][2]string, err error) {
+	var seq iter.Seq2[js.Value[T], error]
+	if seq, err = iterate(val); err != nil {
+		return
 	}
-
-	itRes, err := itFn.Call(arg)
-	if err != nil {
-		return nil, err
-	}
-	obj, ok := itRes.AsObject()
-	if !ok {
-		return nil, errors.New("Bad iterator object")
-	}
-
-	res := make([][2]string, 0)
-	next, err := obj.Get("next")
-	if err != nil {
-		return nil, err
-	}
-	if next == nil || next.IsUndefined() {
-		keys, _ := obj.Keys()
-		return nil, fmt.Errorf("Iterator doesn't have function 'next'. Keys: %+v", keys)
-	}
-	fn, ok := next.AsFunction()
-	if !ok {
-		return nil, errors.New("Next is not a function")
-	}
-	for {
-		result, err := fn.Call(obj)
+	for v, err := range seq {
 		if err != nil {
-			return nil, fmt.Errorf("next: %w", err)
+			return nil, err
 		}
-		resultObj, ok := result.AsObject()
+		obj, ok := v.AsObject()
 		if !ok {
-			break
+			return nil, scope.NewTypeError("Not an object")
 		}
-		done, err := resultObj.Get("done")
-		if err != nil {
-			return nil, fmt.Errorf("done: %w", err)
-		}
-		if done.Boolean() {
-			break
-		}
-		value, err := resultObj.Get("value")
-		if err != nil {
-			return nil, fmt.Errorf("value: %w", err)
-		}
-		valObj, ok := value.AsObject()
-		if !ok {
-			break
-		}
-		v1, err1 := valObj.Get("0")
-		v2, err2 := valObj.Get("1")
+		v1, err1 := obj.Get("0")
+		v2, err2 := obj.Get("1")
 		if err = errors.Join(err1, err2); err != nil {
 			return nil, err
 		}
@@ -119,7 +73,78 @@ func (w Headers[T]) parseHeaderIterator(
 			v2.String(),
 		})
 	}
-	return res, nil
+	return
+}
+
+// iterate returns a seq.Iter2 exposing a JavaScript iterable as a Go iterator.
+// It will return an ErrNotIterable error if the JavaScript value is not an
+// object implementing the [Iterable] protocol. An error is returned if
+// obtaining the [Iterator] itself resulted in an error. The returned Seq will
+// return yield an error value if the JavaScript iterator throws an error during
+// iteration.
+//
+// [Iterable]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols#the_iterable_protocol
+// [Iterator]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols#the_iterator_protocol
+func iterate[T any](v js.Value[T]) (iter.Seq2[js.Value[T], error], error) {
+	obj, ok := v.AsObject()
+	if !ok {
+		return nil, ErrNotIterable
+	}
+	iteratorVal, err := obj.Iterator()
+	if err != nil {
+		return nil, err
+	}
+	symIter, ok := iteratorVal.AsFunction()
+	if !ok {
+		return nil, ErrNotIterable
+	}
+	iterVal, err := symIter.Call(obj)
+	if err != nil {
+		return nil, err
+	}
+	iter, ok := iterVal.AsObject()
+	if !ok {
+		return nil, ErrNotIterable
+	}
+	nextVal, err := iter.Get("next")
+	if err != nil {
+		return nil, err
+	}
+	next, ok := nextVal.AsFunction()
+	if !ok {
+		return nil, errNotIterable("next is not a function")
+	}
+	return func(yield func(js.Value[T], error) bool) {
+		for {
+			result, err := next.Call(iter)
+			if err != nil {
+				yield(nil, err)
+				return
+			}
+			resultObj, ok := result.AsObject()
+			if !ok {
+				break
+			}
+			done, err := resultObj.Get("done")
+			if err != nil {
+				yield(nil, err)
+				return
+			}
+			if done.Boolean() {
+				return
+			}
+			val, err := resultObj.Get("value")
+			if err != nil || !yield(val, err) {
+				return
+			}
+		}
+	}, nil
+}
+
+var ErrNotIterable = errors.New("gost-dom/scripting: value not iterable")
+
+func errNotIterable(msg string) error {
+	return fmt.Errorf("%w: %s", ErrNotIterable, msg)
 }
 
 func (w Headers[T]) CustomInitializer(jsClass js.Class[T]) {
