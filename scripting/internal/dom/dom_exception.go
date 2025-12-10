7008cf71e8d4ff045160c265644b5cacc7d4825e
Peter Strøiman <peter@stroiman.com>
2025-12-10 16:47:38 +0100

    Extract generic error decode logic

--- scripting/internal/dom/dom_exception.go
+++ scripting/internal/dom/dom_exception.go
@@ -21,38 +21,41 @@ func (w domException[T]) Initialize(jsClass js.Class[T]) {
 	// TODO: Check if the message shouldn't have been Error.prototype.message
 	jsClass.CreateInstanceAttribute("message", w.message, nil)
 }
-func (w domException[T]) code(cbCtx js.CallbackContext[T]) (res js.Value[T], err error) {
-	val, err := js.As[error](cbCtx.Instance())
+
+// decodeThisError retrieves the wrapped error object from the `this` object
+func decodeThisError[T any, E error](cbCtx js.CallbackContext[T], res *E) (err error) {
+	var val error
+	val, err = js.As[error](cbCtx.Instance())
 	if err != nil {
 		return
 	}
-	var domErr dom.DOMError
-	if !errors.As(val, &domErr) {
-		return nil, nil
+	if !errors.As(val, res) {
+		err = cbCtx.NewTypeError("Value does not implement DOMException")
+		return
 	}
-	return cbCtx.NewInt32(int32(domErr.Code)), nil
+	return
+}
+
+func (w domException[T]) code(cbCtx js.CallbackContext[T]) (res js.Value[T], err error) {
+	var domErr dom.DOMError
+	if err = decodeThisError(cbCtx, &domErr); err == nil {
+		res = cbCtx.NewInt32(int32(domErr.Code))
+	}
+	return
 }
 
 func (w domException[T]) name(cbCtx js.CallbackContext[T]) (res js.Value[T], err error) {
-	val, err := js.As[error](cbCtx.Instance())
-	if err != nil {
-		return
-	}
 	var domErr dom.DOMError
-	if !errors.As(val, &domErr) {
-		return nil, nil
+	if err = decodeThisError(cbCtx, &domErr); err == nil {
+		res = cbCtx.NewString(domErr.Code.String())
 	}
-	return cbCtx.NewString(domErr.Code.String()), nil
+	return
 }
 
 func (w domException[T]) message(cbCtx js.CallbackContext[T]) (res js.Value[T], err error) {
-	val, err := js.As[error](cbCtx.Instance())
-	if err != nil {
-		return
-	}
 	var domErr dom.DOMError
-	if !errors.As(val, &domErr) {
-		return nil, nil
+	if err = decodeThisError(cbCtx, &domErr); err == nil {
+		res = cbCtx.NewString(domErr.Message)
 	}
-	return cbCtx.NewString(domErr.Message), nil
+	return
 }
