573e405ff65dd74fca7980410bd54c558d13c8bc
Peter Strøiman <peter@stroiman.com>
2025-12-22 18:14:41 +0100

    Fix typo in package docs

--- internal/cache/package.go
+++ internal/cache/package.go
@@ -1,4 +1,4 @@
-// Packace cache provides the ability to cache expensive resources
+// Package cache provides the ability to cache expensive resources
 //
 // The cache is intended to not only cache HTTP response; but also expensive
 // processing performed on the response. E.g., parsing JavaScript into a
