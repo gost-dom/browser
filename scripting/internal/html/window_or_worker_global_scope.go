6a6aa3eb7e40443592418dd890d3e2f429076809
Peter Strøiman <peter@stroiman.com>
2025-12-27 06:42:47 +0100

    Remove unnecessary log statements

--- scripting/internal/html/window_or_worker_global_scope.go
+++ scripting/internal/html/window_or_worker_global_scope.go
@@ -54,7 +54,6 @@ func (w WindowOrWorkerGlobalScope[T]) setTimeout(cbCtx js.CallbackContext[T]) (j
 func (w WindowOrWorkerGlobalScope[T]) setInterval(
 	cbCtx js.CallbackContext[T],
 ) (js.Value[T], error) {
-	cbCtx.Logger().Debug("JS Function call: queueMicrotask", js.ThisLogAttr(cbCtx))
 	f, err1 := js.ConsumeArgument(cbCtx, "callback", nil, codec.DecodeFunction)
 	delay, err2 := js.ConsumeArgument(cbCtx, "delay", nil, codec.DecodeInt)
 	err := errors.Join(err1, err2)
@@ -75,7 +74,6 @@ func (w WindowOrWorkerGlobalScope[T]) setInterval(
 func (w WindowOrWorkerGlobalScope[T]) clearTimeout(
 	cbCtx js.CallbackContext[T],
 ) (js.Value[T], error) {
-	cbCtx.Logger().Debug("JS Function call: clearTimeout", js.ThisLogAttr(cbCtx))
 	handle, err := js.ConsumeArgument(cbCtx, "handle", nil, codec.DecodeInt)
 	if err == nil {
 		cbCtx.Clock().Cancel(clock.TaskHandle(handle))
@@ -86,7 +84,6 @@ func (w WindowOrWorkerGlobalScope[T]) clearTimeout(
 func (w WindowOrWorkerGlobalScope[T]) clearInterval(
 	cbCtx js.CallbackContext[T],
 ) (js.Value[T], error) {
-	cbCtx.Logger().Debug("JS Function call: clearInterval", js.ThisLogAttr(cbCtx))
 	handle, err := js.ConsumeArgument(cbCtx, "handle", nil, codec.DecodeInt)
 	if err == nil {
 		cbCtx.Clock().Cancel(clock.TaskHandle(handle))
@@ -97,7 +94,6 @@ func (w WindowOrWorkerGlobalScope[T]) clearInterval(
 func (w WindowOrWorkerGlobalScope[T]) queueMicrotask(
 	cbCtx js.CallbackContext[T],
 ) (js.Value[T], error) {
-	cbCtx.Logger().Debug("JS Function call: queueMicrotask", js.ThisLogAttr(cbCtx))
 	f, err := js.ConsumeArgument(cbCtx, "callback", nil, codec.DecodeFunction)
 	if err == nil {
 		clock := cbCtx.Clock()
