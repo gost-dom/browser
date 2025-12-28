9b7c9c281b3ecc9246b7795b9bb955ce7bc824e9
Peter Strøiman <peter@stroiman.com>
2025-12-28 11:41:49 +0100

    Remove dead code

--- scripting/internal/fetch/body.go
+++ scripting/internal/fetch/body.go
@@ -9,13 +9,10 @@ import (
 )
 
 func (w Body[T]) json(cbCtx js.CallbackContext[T]) (res js.Value[T], err error) {
-	p := cbCtx.NewPromise()
-	res = p
 	instance, err := js.As[fetch.Body](cbCtx.Instance())
 	if err != nil {
 		return nil, err
 	}
-	cbCtx.Logger().Debug("js/Body.json")
 	return codec.EncodePromise(cbCtx, promise.ReadAll(instance), EncodeJSONBytes)
 }
 
