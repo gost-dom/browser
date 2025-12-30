78c255e8237af738d32c3ca45e5e00b25de3ae20
Peter Strøiman <peter@stroiman.com>
2025-12-30 10:34:52 +0100

    Remove unused method

--- scripting/internal/fetch/response.go
+++ scripting/internal/fetch/response.go
@@ -3,18 +3,9 @@ package fetch
 import (
 	"fmt"
 
-	"github.com/gost-dom/browser/internal/fetch"
 	"github.com/gost-dom/browser/scripting/internal/js"
 )
 
 func ResponseConstructor[T any](cbCtx js.CallbackContext[T]) (js.Value[T], error) {
 	return nil, fmt.Errorf("gost-dom/fetch: Response constructor not implemented")
-}
-
-func (wrapper Response[T]) toHeaders(
-	ctx js.CallbackContext[T],
-	headers *fetch.Headers,
-) (js.Value[T], error) {
-	ctor := ctx.Constructor("Headers")
-	return ctor.NewInstance(headers)
 }
