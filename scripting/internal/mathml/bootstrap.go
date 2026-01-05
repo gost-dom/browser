e88248cf8f536159694a957196beba013a19a696
Peter Strøiman <peter@stroiman.com>
2026-01-05 08:39:45 +0100

    Remove unnecessary type parameter

--- scripting/internal/mathml/bootstrap.go
+++ scripting/internal/mathml/bootstrap.go
@@ -3,5 +3,5 @@ package mathml
 import js "github.com/gost-dom/browser/scripting/internal/js"
 
 func ConfigureScriptEngine[T any](e js.ScriptEngine[T]) {
-	InitializeMathMlElement[T](js.CreateClass(e, "MathMLElement", "Element", js.IllegalConstructor))
+	InitializeMathMlElement(js.CreateClass(e, "MathMLElement", "Element", js.IllegalConstructor))
 }
