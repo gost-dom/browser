934712cc4ea04139aafffa108aadf9582785a232
Peter Strøiman <peter@stroiman.com>
2026-01-05 19:58:52 +0100

    Create type to represent a realm

---
+++ internal/code-gen/scripting/realm.go
@@ -0,0 +1,26 @@
+package scripting
+
+import (
+	"slices"
+
+	"github.com/gost-dom/webref/idl"
+)
+
+// realm represents an isolated JavaScript environment with it's own set of
+// values available in global scope. E.g., the initial realm, where scripts on
+// the page are executed, have the Window object as global object. Workers run
+// in separate realms and have different globals, e.g.
+// DedicatedWorkerGlobalScope.
+type realm struct {
+	global idl.Interface
+}
+
+// exposes returns whether interface i is exposed in the realm.
+func (r realm) exposes(i idl.Interface) bool {
+	for _, global := range r.global.Global {
+		if slices.Contains(i.Exposed, global) {
+			return true
+		}
+	}
+	return false
+}
