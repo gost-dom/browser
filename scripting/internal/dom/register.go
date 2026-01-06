ad3bac190314870d37a6ab1fa7f91eb4e5eb6f1f
Peter Strøiman <peter@stroiman.com>
2026-01-06 23:26:43 +0100

    Cleanup DOM registration

--- scripting/internal/dom/register.go
+++
@@ -1,8 +0,0 @@
-package dom
-
-import js "github.com/gost-dom/browser/scripting/internal/js"
-
-func Register[T any](e js.ScriptEngine[T]) {
-	InitializeDomException(js.CreateClass(e, "DOMException", "", domExceptionConstructor))
-	Bootstrap(e)
-}
