eb0bcb84ad3c4dd2fdd003a3437fd35c8e678336
Peter Strøiman <peter@stroiman.com>
2026-01-06 23:26:43 +0100

    Cleanup DOM registration

--- scripting/internal/dom/configurator.go
+++ scripting/internal/dom/configurator.go
@@ -4,7 +4,8 @@ import "github.com/gost-dom/browser/scripting/internal/js"
 
 func ConfigureScriptEngine[T any](e js.ScriptEngine[T]) {
 	installDOMParser(e)
-	Register(e)
+	InitializeDomException(js.CreateClass(e, "DOMException", "", domExceptionConstructor))
+	Bootstrap(e)
 	js.CreateClass(e, "ShadowRoot", "DocumentFragment", shadowRootConstructor)
 	InitializeCustomEvent(js.CreateClass(e, "CustomEvent", "Event", customEventConstructor))
 }
