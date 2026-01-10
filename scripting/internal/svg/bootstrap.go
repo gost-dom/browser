ef0e099b2c37eba9e909d8b2177680c1ef25b80f
Peter Strøiman <peter@stroiman.com>
2026-01-10 13:12:19 +0100

    Fix codedoc typo

--- scripting/internal/svg/bootstrap.go
+++ scripting/internal/svg/bootstrap.go
@@ -3,7 +3,7 @@ package svg
 import js "github.com/gost-dom/browser/scripting/internal/js"
 
 func ConfigureScriptEngine[T any](e js.ScriptEngine[T]) {
-	// The SVGElement is added for Datastar support, which checks en element
+	// The SVGElement is added for Datastar support, which checks an element
 	// is an instanceof SVGElement. Would throw a JavaScript error if not
 	// present in global scope.
 	InitializeSvgElement(js.CreateClass(e, "SVGElement", "Element", js.IllegalConstructor))
