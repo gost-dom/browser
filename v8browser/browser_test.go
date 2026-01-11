95980a51c6adc0f8807b41393b6b756613a47270
Peter Strøiman <peter@stroiman.com>
2026-01-11 13:06:34 +0100

    Improve cookie handling

--- v8browser/browser_test.go
+++ v8browser/browser_test.go
@@ -224,7 +224,10 @@ func cookieHandler(w http.ResponseWriter, r *http.Request) {
 	if c, _ := r.Cookie("gost"); c != nil {
 		gost = c.Value
 	}
-	w.Header().Set("Set-Cookie", "gost=Hello, World!")
+	http.SetCookie(w, &http.Cookie{
+		Name:  "gost",
+		Value: "Hello, World!",
+	})
 	fmt.Fprintf(w, `<body><div id="gost">%s</div></body>`, gost)
 }
 
