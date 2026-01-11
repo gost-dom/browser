12e8fd0ece71a19ca704692e174a7b61e8ed3af1
Peter Strøiman <peter@stroiman.com>
2026-01-11 11:55:21 +0100

    Consolidate test suite

--- scripting/internal/scripttests/event_loop_suite.go
+++ scripting/internal/scripttests/event_loop_suite.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/gost-dom/browser/html"
 	"github.com/gost-dom/browser/internal/testing/browsertest"
-	"github.com/gost-dom/browser/testing/gosttest"
+	"github.com/gost-dom/browser/internal/testing/htmltest"
 	"github.com/onsi/gomega"
 	. "github.com/onsi/gomega"
 	"github.com/stretchr/testify/suite"
@@ -13,8 +13,9 @@ import (
 
 type EventLoopTestSuite struct {
 	suite.Suite
-	e   html.ScriptEngine
-	ctx html.ScriptContext
+	e html.ScriptEngine
+	// ctx html.ScriptContext
+	w htmltest.WindowHelper
 }
 
 func NewEventLoopTestSuite(e html.ScriptEngine) *EventLoopTestSuite {
@@ -22,46 +23,44 @@ func NewEventLoopTestSuite(e html.ScriptEngine) *EventLoopTestSuite {
 }
 
 func (s *EventLoopTestSuite) SetupTest() {
-	w := browsertest.InitWindow(s.T(), s.e)
-	s.ctx = w.ScriptContext()
+	s.w = browsertest.InitWindow(s.T(), s.e, browsertest.WithIgnoreErrorLogs())
 }
 
 func (s *EventLoopTestSuite) TeardownTest() {
-	s.ctx.Close()
+	s.w.Close()
 }
 
 func (s *EventLoopTestSuite) TestDeferExecution() {
 	Expect := gomega.NewWithT(s.T()).Expect
 	Expect(
-		s.ctx.Eval(`
+		s.w.Eval(`
 				let val; 
 				setTimeout(() => { val = 42 }, 1);
 				val`,
 		),
 	).To(BeNil())
-	s.ctx.Clock().Advance(time.Millisecond)
-	Expect(s.ctx.Eval(`val`)).To(BeEquivalentTo(42))
+	s.w.Clock().Advance(time.Millisecond)
+	Expect(s.w.Eval(`val`)).To(BeEquivalentTo(42))
 }
 
 func (s *EventLoopTestSuite) TestClearTimeout() {
 	Expect := gomega.NewWithT(s.T()).Expect
 	Expect(
-		s.ctx.Eval(`
+		s.w.Eval(`
 				let val;
 				let handle = setTimeout(() => { val = 42 }, 1);
 				clearTimeout(handle);
 				val`,
 		),
 	).To(BeNil())
-	s.ctx.Clock().Advance(time.Millisecond)
-	Expect(s.ctx.Eval(`val`)).To(BeNil())
+	s.w.Clock().Advance(time.Millisecond)
+	Expect(s.w.Eval(`val`)).To(BeNil())
 }
 
 func (s *EventLoopTestSuite) TestDispatchError() {
 	Expect := gomega.NewWithT(s.T()).Expect
-	w := browsertest.InitWindow(s.T(), s.e, browsertest.WithLogOption(gosttest.AllowErrors()))
 	Expect(
-		w.MustEval(`
+		s.w.MustEval(`
 				let val;
 				window.addEventListener('error', () => {
 					val = 42;
@@ -72,13 +71,13 @@ func (s *EventLoopTestSuite) TestDispatchError() {
 				val`,
 		),
 	).To(BeNil())
-	Expect(w.MustEval(`val`)).To(BeEquivalentTo(42))
+	Expect(s.w.MustEval(`val`)).To(BeEquivalentTo(42))
 }
 
 func (s *EventLoopTestSuite) TestQueueMicrotask() {
 	Expect := gomega.NewWithT(s.T()).Expect
 	Expect(
-		s.ctx.Eval(`
+		s.w.Eval(`
 				window.taskExecuted = false;
 				window.queueMicrotask(() => {
 					window.taskExecuted = true;
@@ -86,31 +85,31 @@ func (s *EventLoopTestSuite) TestQueueMicrotask() {
 				window.taskExecuted `,
 		),
 	).To(BeFalse())
-	Expect(s.ctx.Eval(`window.taskExecuted`)).To(BeTrue())
+	Expect(s.w.Eval(`window.taskExecuted`)).To(BeTrue())
 }
 
 func (s *EventLoopTestSuite) TestInterval() {
 	Expect := gomega.NewWithT(s.T()).Expect
-	Expect(s.ctx.Eval(`
+	Expect(s.w.Eval(`
 		let count = 0;
 		const h = globalThis.setInterval(() => {
 			count++;
 		}, 100);
 		count
 	`)).To(BeEquivalentTo(0))
-	s.ctx.Clock().Advance(100 * time.Millisecond)
-	Expect(s.ctx.Eval("count")).To(BeEquivalentTo(1))
-	s.ctx.Clock().Advance(300 * time.Millisecond)
-	Expect(s.ctx.Eval("count")).To(BeEquivalentTo(4))
-	s.ctx.Run("clearInterval(h)")
-	s.ctx.Clock().Advance(300 * time.Millisecond)
-	Expect(s.ctx.Eval("count")).To(BeEquivalentTo(4))
+	s.w.Clock().Advance(100 * time.Millisecond)
+	Expect(s.w.Eval("count")).To(BeEquivalentTo(1))
+	s.w.Clock().Advance(300 * time.Millisecond)
+	Expect(s.w.Eval("count")).To(BeEquivalentTo(4))
+	s.w.Run("clearInterval(h)")
+	s.w.Clock().Advance(300 * time.Millisecond)
+	Expect(s.w.Eval("count")).To(BeEquivalentTo(4))
 }
 
 func (s *EventLoopTestSuite) TestGlobals() {
 	// Methods should be in the window _instance_ not the Window prototype
 	Expect := gomega.NewWithT(s.T()).Expect
-	windowNames, err := s.ctx.Eval("Object.getOwnPropertyNames(window)")
+	windowNames, err := s.w.Eval("Object.getOwnPropertyNames(window)")
 	Expect(err).ToNot(HaveOccurred())
 	Expect(windowNames).To(ContainElement("setTimeout"))
 	Expect(windowNames).To(ContainElement("clearTimeout"))
@@ -118,7 +117,7 @@ func (s *EventLoopTestSuite) TestGlobals() {
 	Expect(windowNames).To(ContainElement("clearInterval"))
 	Expect(windowNames).To(ContainElement("queueMicrotask"))
 
-	prototypeNames, err := s.ctx.Eval("Object.getOwnPropertyNames(Window.prototype)")
+	prototypeNames, err := s.w.Eval("Object.getOwnPropertyNames(Window.prototype)")
 	Expect(err).ToNot(HaveOccurred())
 	Expect(prototypeNames).ToNot(ContainElement("setTimeout"))
 	Expect(prototypeNames).ToNot(ContainElement("clearTimeout"))
