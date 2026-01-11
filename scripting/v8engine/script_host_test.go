72bc34f7b7c5090dd417692bac9646b575a198d0
Peter Strøiman <peter@stroiman.com>
2026-01-11 12:38:44 +0100

    Remove dependency to deprecated New

--- scripting/v8engine/script_host_test.go
+++ scripting/v8engine/script_host_test.go
@@ -1,10 +1,9 @@
 package v8engine
 
 import (
-	"strings"
 	"testing"
 
-	"github.com/gost-dom/browser/html"
+	"github.com/gost-dom/browser/internal/testing/browsertest"
 	. "github.com/gost-dom/browser/internal/testing/gomega-matchers"
 	"github.com/gost-dom/browser/scripting/internal/scripttests"
 	"github.com/onsi/gomega"
@@ -12,20 +11,15 @@ import (
 
 func TestScriptHostDocumentScriptLoading(t *testing.T) {
 	Expect := gomega.NewWithT(t).Expect
-	reader := strings.NewReader(`<html><body>
+	win := browsertest.InitWindow(t, defaultEngine)
+	err := win.LoadHTML(`<html><body>
     <script>window.sut = document.documentElement.outerHTML</script>
     <div>I should not be in the output</div>
   </body></html>
 `)
-	host := New()
-	t.Cleanup(host.Close)
-	options := html.WindowOptions{ScriptHost: host, Clock: host.clock}
-	win, err := html.NewWindowReader(reader, nil, options)
 	Expect(err).ToNot(HaveOccurred())
-	defer win.Close()
-	ctx := win.ScriptContext()
 	Expect(
-		ctx.Eval("window.sut"),
+		win.Eval("window.sut"),
 	).To(Equal(`<html><head></head><body>
     <script>window.sut = document.documentElement.outerHTML</script></body></html>`))
 }
